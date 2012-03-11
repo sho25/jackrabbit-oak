693d223a97c7cb5665c2de24e7ab00245eaf5b58
Michael Dürig <mduerig@apache.org>
2012-03-11 16:12:53 +0000

    OAK-8: Make return types of NodeState#getProperties() and NodeState#getChildNodeEntries() covariant
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1299385 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3e755127d2c1db87555ddccd033d7812b25e0ce5

--- oak-core/src/main/java/org/apache/jackrabbit/oak/model/NodeState.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/model/NodeState.java
@@ -42,6 +42,8 @@ function_decl|;
 comment|/**      * Returns an iterable of the properties of this node. Multiple      * iterations are guaranteed to return the properties in the same      * order, but the specific order used is implementation-dependent      * and may change across different states of the same node.      *      * @return properties in some stable order      */
 name|Iterable
 argument_list|<
+name|?
+extends|extends
 name|PropertyState
 argument_list|>
 name|getProperties
@@ -63,6 +65,8 @@ function_decl|;
 comment|/**      * Returns an iterable of the child node entries starting from the      * given offset. Multiple iterations are guaranteed to return the      * child nodes in the same order, but the specific order used is      * implementation-dependent and may change across different states      * of the same node. An empty iterable is returned if the given      * offset is greater than the offset of the last child node entry.      *      * @param offset zero-based offset of the first entry to return      * @param length maximum number of entries to return,      *               or -1 for all remaining entries      * @return requested child node entries in some stable order      */
 name|Iterable
 argument_list|<
+name|?
+extends|extends
 name|ChildNodeEntry
 argument_list|>
 name|getChildNodeEntries
