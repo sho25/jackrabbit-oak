74f810621be9ed09b8bfd0b24d47a3a8ecdd68a7
Thomas Mueller <thomasm@apache.org>
2012-03-20 15:05:04 +0000

    OAK-28 Query implementation (moved from the sandbox)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1302927 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4f4f1b2e41b68a11ec32ca50b543ce44a7e66d7a

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/query/index/NodeReader.java
@@ -0,0 +1,62 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *   http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|query
+operator|.
+name|index
+package|;
+end_package
+
+begin_comment
+comment|/**  * A node reader. The reader should use the data in the filter if possible to  * speed up reading.  */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|NodeReader
+block|{
+comment|/**      * Estimate the cost to use this reader with the given filter. The returned      * cost is a value between 1 (very fast; lookup of a unique node) and the      * estimated number of nodes to traverse.      *      * @param filter the filter      * @return the estimated cost in number of read nodes      */
+name|double
+name|getCost
+parameter_list|(
+name|Filter
+name|filter
+parameter_list|)
+function_decl|;
+comment|/**      * Start reading nodes.      *      * @param filter the filter      * @param revisionId the revision      * @return a cursor to iterate over the result      */
+name|Cursor
+name|query
+parameter_list|(
+name|Filter
+name|filter
+parameter_list|,
+name|String
+name|revisionId
+parameter_list|)
+function_decl|;
+comment|/**      * Get the query plan for the given reader.      *      * @param filter the filter      * @return the query plan      */
+name|String
+name|getPlan
+parameter_list|(
+name|Filter
+name|filter
+parameter_list|)
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
