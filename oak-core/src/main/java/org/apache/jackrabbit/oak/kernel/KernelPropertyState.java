0de6527536bec6248376598317713361944e0b87
Michael Dürig <mduerig@apache.org>
2012-04-10 13:45:12 +0000

    OAK-15: Clean up oak-jcr make KernelPropertyState package private again
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1311722 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c114eaf57a20e83a417eed69d541e567e8e01a0a

--- oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/KernelPropertyState.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/KernelPropertyState.java
@@ -70,13 +70,11 @@ import|;
 end_import
 
 begin_class
-specifier|public
 class|class
 name|KernelPropertyState
 extends|extends
 name|AbstractPropertyState
 block|{
-comment|// fixme make package private
 specifier|private
 specifier|final
 name|String
