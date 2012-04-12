87d5da7ad3fd1e6170bf12296bfd8191bd2e80a9
Julian Reschke <reschke@apache.org>
2012-04-12 13:02:09 +0000

    OAK-60: add a short sleep before checking the GC (needs review)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1325234 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5c9f876b2852e4ec9b1b56d09f70a3d86da4b5af

--- oak-mk/src/test/java/org/apache/jackrabbit/mk/blobs/DbBlobStoreTest.java
+++ oak-mk/src/test/java/org/apache/jackrabbit/mk/blobs/DbBlobStoreTest.java
@@ -1254,6 +1254,23 @@ operator|.
 name|clearCache
 argument_list|()
 expr_stmt|;
+comment|// https://issues.apache.org/jira/browse/OAK-60
+try|try
+block|{
+name|Thread
+operator|.
+name|sleep
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|InterruptedException
+name|e
+parameter_list|)
+block|{          }
 name|store
 operator|.
 name|startMark
@@ -1349,11 +1366,6 @@ operator|>
 literal|0
 argument_list|)
 expr_stmt|;
-comment|//        try {
-comment|//            Thread.sleep(10);
-comment|//        } catch (InterruptedException e) {
-comment|//
-comment|//        }
 block|}
 specifier|private
 name|void
