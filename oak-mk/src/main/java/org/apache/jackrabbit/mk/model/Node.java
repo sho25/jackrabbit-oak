20f49932b17f050110edc116329870a94bc38dc9
Angela Schreiber <angela@apache.org>
2012-06-06 08:22:56 +0000

    remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1346773 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6d21e346a4bdf93abd8fe4ab84b463853da22068

--- oak-mk/src/main/java/org/apache/jackrabbit/mk/model/Node.java
+++ oak-mk/src/main/java/org/apache/jackrabbit/mk/model/Node.java
@@ -35,22 +35,6 @@ end_import
 
 begin_import
 import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|jackrabbit
-operator|.
-name|mk
-operator|.
-name|store
-operator|.
-name|NotFoundException
-import|;
-end_import
-
-begin_import
-import|import
 name|java
 operator|.
 name|util
