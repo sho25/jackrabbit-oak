a22afad67d3a6d54cb4750bfbb8aa3ecf1c023e9
Jukka Zitting <jukka@apache.org>
2012-07-12 10:23:27 +0000

    OAK-167: Support orderable nodes
    Add initial support for iterating over explicitly ordered nodes.
    Simplify AbstractRepositoryTest
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1360617 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: dfde12593632728dc2d9c3f2ada117f3cabf1de0

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/CompatibilityIssuesTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/CompatibilityIssuesTest.java
@@ -90,19 +90,6 @@ name|CompatibilityIssuesTest
 extends|extends
 name|AbstractRepositoryTest
 block|{
-annotation|@
-name|After
-specifier|public
-name|void
-name|tearDown
-parameter_list|()
-throws|throws
-name|RepositoryException
-block|{
-name|logout
-argument_list|()
-expr_stmt|;
-block|}
 comment|/**      * Trans-session isolation differs from Jackrabbit 2. Snapshot isolation can      * result in write skew as this test demonstrates: the check method enforces      * an application logic constraint which says that the sum of the properties      * p1 and p2 must not be negative. While session1 and session2 each enforce      * this constraint before saving, the constraint might not hold globally as      * can be seen in session3.      *      * @see<a href="http://wiki.apache.org/jackrabbit/Transactional%20model%20of%20the%20Microkernel%20based%20Jackrabbit%20prototype">      *     Transactional model of the Microkernel based Jackrabbit prototype</a>      */
 annotation|@
 name|Test
