c577918dcc83f2b3c0da26ac5a08ea583d86a7b4
Alexandru Parvulescu <alexparvulescu@apache.org>
2012-11-12 16:34:55 +0000

    OAK-438 Provide the lucene index as an osgi bundle  - enabled the query tests
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1408345 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 60e27f5b7c6e1cc58aa5a84ac09dd01e7f94dcbf

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexQueryTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexQueryTest.java
@@ -1,479 +1,586 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|///*
-end_comment
-
-begin_comment
-comment|// * Licensed to the Apache Software Foundation (ASF) under one or more
-end_comment
-
-begin_comment
-comment|// * contributor license agreements.  See the NOTICE file distributed with
-end_comment
-
-begin_comment
-comment|// * this work for additional information regarding copyright ownership.
-end_comment
-
-begin_comment
-comment|// * The ASF licenses this file to You under the Apache License, Version 2.0
-end_comment
-
-begin_comment
-comment|// * (the "License"); you may not use this file except in compliance with
-end_comment
-
-begin_comment
-comment|// * the License.  You may obtain a copy of the License at
-end_comment
-
-begin_comment
-comment|// *
-end_comment
-
-begin_comment
-comment|// *      http://www.apache.org/licenses/LICENSE-2.0
-end_comment
-
-begin_comment
-comment|// *
-end_comment
-
-begin_comment
-comment|// * Unless required by applicable law or agreed to in writing, software
-end_comment
-
-begin_comment
-comment|// * distributed under the License is distributed on an "AS IS" BASIS,
-end_comment
-
-begin_comment
-comment|// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-end_comment
-
-begin_comment
-comment|// * See the License for the specific language governing permissions and
-end_comment
-
-begin_comment
-comment|// * limitations under the License.
-end_comment
-
-begin_comment
-comment|// */
-end_comment
-
-begin_comment
-comment|//package org.apache.jackrabbit.oak.plugins.index.lucene;
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//import static junit.framework.Assert.assertEquals;
-end_comment
-
-begin_comment
-comment|//import static org.junit.Assert.assertFalse;
-end_comment
-
-begin_comment
-comment|//import static org.junit.Assert.assertTrue;
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//import java.util.Iterator;
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//import org.apache.jackrabbit.oak.Oak;
-end_comment
-
-begin_comment
-comment|//import org.apache.jackrabbit.oak.api.ContentRepository;
-end_comment
-
-begin_comment
-comment|//import org.apache.jackrabbit.oak.api.Tree;
-end_comment
-
-begin_comment
-comment|//import org.apache.jackrabbit.oak.plugins.index.IndexHookManager;
-end_comment
-
-begin_comment
-comment|//import org.apache.jackrabbit.oak.plugins.nodetype.InitialContent;
-end_comment
-
-begin_comment
-comment|//import org.apache.jackrabbit.oak.query.AbstractQueryTest;
-end_comment
-
-begin_comment
-comment|//import org.apache.jackrabbit.oak.query.JsopUtil;
-end_comment
-
-begin_comment
-comment|//import org.junit.Ignore;
-end_comment
-
-begin_comment
-comment|//import org.junit.Test;
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|///**
-end_comment
-
-begin_comment
-comment|// * Tests the query engine using the default index implementation: the
-end_comment
-
-begin_comment
-comment|// * {@link LuceneIndexProvider}
-end_comment
-
-begin_comment
-comment|// */
-end_comment
-
-begin_comment
-comment|//public class LuceneIndexQueryTest extends AbstractQueryTest {
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//    @Override
-end_comment
-
-begin_comment
-comment|//    protected void createTestIndexNode() throws Exception {
-end_comment
-
-begin_comment
-comment|//        Tree index = root.getTree("/");
-end_comment
-
-begin_comment
-comment|//        createTestIndexNode(index, LuceneIndexConstants.TYPE_LUCENE);
-end_comment
-
-begin_comment
-comment|//        root.commit();
-end_comment
-
-begin_comment
-comment|//    }
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//    @Override
-end_comment
-
-begin_comment
-comment|//    protected ContentRepository createRepository() {
-end_comment
-
-begin_comment
-comment|//        return new Oak()
-end_comment
-
-begin_comment
-comment|//            .with(new InitialContent())
-end_comment
-
-begin_comment
-comment|//            .with(new LuceneIndexProvider())
-end_comment
-
-begin_comment
-comment|//            .with(new IndexHookManager(new LuceneIndexHookProvider()))
-end_comment
-
-begin_comment
-comment|//            .createContentRepository();
-end_comment
-
-begin_comment
-comment|//    }
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//    @Test
-end_comment
-
-begin_comment
-comment|//    @Ignore("OAK-420")
-end_comment
-
-begin_comment
-comment|//    public void sql2() throws Exception {
-end_comment
-
-begin_comment
-comment|//        test("sql2.txt");
-end_comment
-
-begin_comment
-comment|//    }
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//    @Test
-end_comment
-
-begin_comment
-comment|//    @Ignore("OAK-420")
-end_comment
-
-begin_comment
-comment|//    public void sql2Measure() throws Exception {
-end_comment
-
-begin_comment
-comment|//        test("sql2_measure.txt");
-end_comment
-
-begin_comment
-comment|//    }
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//    @Test
-end_comment
-
-begin_comment
-comment|//    public void descendantTest() throws Exception {
-end_comment
-
-begin_comment
-comment|//        JsopUtil.apply(root, "/ + \"test\": { \"a\": {}, \"b\": {} }");
-end_comment
-
-begin_comment
-comment|//        root.commit();
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//        Iterator<String> result = executeQuery(
-end_comment
-
-begin_comment
-comment|//                "select * from [nt:base] where isdescendantnode('/test')",
-end_comment
-
-begin_comment
-comment|//                "JCR-SQL2").iterator();
-end_comment
-
-begin_comment
-comment|//        assertTrue(result.hasNext());
-end_comment
-
-begin_comment
-comment|//        assertEquals("/test/a", result.next());
-end_comment
-
-begin_comment
-comment|//        assertEquals("/test/b", result.next());
-end_comment
-
-begin_comment
-comment|//        assertFalse(result.hasNext());
-end_comment
-
-begin_comment
-comment|//    }
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//    @Test
-end_comment
-
-begin_comment
-comment|//    public void descendantTest2() throws Exception {
-end_comment
-
-begin_comment
-comment|//        JsopUtil.apply(
-end_comment
-
-begin_comment
-comment|//                root,
-end_comment
-
-begin_comment
-comment|//                "/ + \"test\": { \"a\": { \"name\": [\"Hello\", \"World\" ] }, \"b\": { \"name\" : \"Hello\" }}");
-end_comment
-
-begin_comment
-comment|//        root.commit();
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//        Iterator<String> result = executeQuery(
-end_comment
-
-begin_comment
-comment|//                "select * from [nt:base] where isdescendantnode('/test') and name='World'",
-end_comment
-
-begin_comment
-comment|//                "JCR-SQL2").iterator();
-end_comment
-
-begin_comment
-comment|//        assertTrue(result.hasNext());
-end_comment
-
-begin_comment
-comment|//        assertEquals("/test/a", result.next());
-end_comment
-
-begin_comment
-comment|//        assertFalse(result.hasNext());
-end_comment
-
-begin_comment
-comment|//    }
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//    @Test
-end_comment
-
-begin_comment
-comment|//    @Ignore("OAK-420")
-end_comment
-
-begin_comment
-comment|//    public void ischildnodeTest() throws Exception {
-end_comment
-
-begin_comment
-comment|//        JsopUtil.apply(
-end_comment
-
-begin_comment
-comment|//                root,
-end_comment
-
-begin_comment
-comment|//                "/ + \"parents\": { \"p0\": {\"id\": \"0\"}, \"p1\": {\"id\": \"1\"}, \"p2\": {\"id\": \"2\"}}");
-end_comment
-
-begin_comment
-comment|//        JsopUtil.apply(
-end_comment
-
-begin_comment
-comment|//                root,
-end_comment
-
-begin_comment
-comment|//                "/ + \"children\": { \"c1\": {\"p\": \"1\"}, \"c2\": {\"p\": \"1\"}, \"c3\": {\"p\": \"2\"}, \"c4\": {\"p\": \"3\"}}");
-end_comment
-
-begin_comment
-comment|//        root.commit();
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|//        Iterator<String> result = executeQuery(
-end_comment
-
-begin_comment
-comment|//                "select * from [nt:base] as p inner join [nt:base] as p2 on ischildnode(p2, p) where p.[jcr:path] = '/'",
-end_comment
-
-begin_comment
-comment|//                "JCR-SQL2").iterator();
-end_comment
-
-begin_comment
-comment|//        assertTrue(result.hasNext());
-end_comment
-
-begin_comment
-comment|//        assertEquals("/, /children", result.next());
-end_comment
-
-begin_comment
-comment|//        assertEquals("/, /jcr:system", result.next());
-end_comment
-
-begin_comment
-comment|//        assertEquals("/, /oak:index", result.next());
-end_comment
-
-begin_comment
-comment|//        assertEquals("/, /parents", result.next());
-end_comment
-
-begin_comment
-comment|//        assertFalse(result.hasNext());
-end_comment
-
-begin_comment
-comment|//    }
-end_comment
-
-begin_comment
-comment|//}
-end_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|plugins
+operator|.
+name|index
+operator|.
+name|lucene
+package|;
+end_package
+
+begin_import
+import|import static
+name|junit
+operator|.
+name|framework
+operator|.
+name|Assert
+operator|.
+name|assertEquals
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertFalse
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertTrue
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Iterator
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|Oak
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|api
+operator|.
+name|ContentRepository
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|api
+operator|.
+name|Tree
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|plugins
+operator|.
+name|index
+operator|.
+name|IndexHookManager
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|plugins
+operator|.
+name|nodetype
+operator|.
+name|InitialContent
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|query
+operator|.
+name|AbstractQueryTest
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|query
+operator|.
+name|JsopUtil
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Test
+import|;
+end_import
+
+begin_comment
+comment|/**  * Tests the query engine using the default index implementation: the  * {@link LuceneIndexProvider}  */
+end_comment
+
+begin_class
+specifier|public
+class|class
+name|LuceneIndexQueryTest
+extends|extends
+name|AbstractQueryTest
+block|{
+annotation|@
+name|Override
+specifier|protected
+name|void
+name|createTestIndexNode
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|Tree
+name|index
+init|=
+name|root
+operator|.
+name|getTree
+argument_list|(
+literal|"/"
+argument_list|)
+decl_stmt|;
+name|createTestIndexNode
+argument_list|(
+name|index
+argument_list|,
+name|LuceneIndexConstants
+operator|.
+name|TYPE_LUCENE
+argument_list|)
+expr_stmt|;
+name|root
+operator|.
+name|commit
+argument_list|()
+expr_stmt|;
+block|}
+annotation|@
+name|Override
+specifier|protected
+name|ContentRepository
+name|createRepository
+parameter_list|()
+block|{
+return|return
+operator|new
+name|Oak
+argument_list|()
+operator|.
+name|with
+argument_list|(
+operator|new
+name|InitialContent
+argument_list|()
+argument_list|)
+operator|.
+name|with
+argument_list|(
+operator|new
+name|LuceneIndexProvider
+argument_list|()
+argument_list|)
+operator|.
+name|with
+argument_list|(
+operator|new
+name|IndexHookManager
+argument_list|(
+operator|new
+name|LuceneIndexHookProvider
+argument_list|()
+argument_list|)
+argument_list|)
+operator|.
+name|createContentRepository
+argument_list|()
+return|;
+block|}
+annotation|@
+name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"OAK-420"
+argument_list|)
+specifier|public
+name|void
+name|sql2
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|test
+argument_list|(
+literal|"sql2.txt"
+argument_list|)
+expr_stmt|;
+block|}
+annotation|@
+name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"OAK-420"
+argument_list|)
+specifier|public
+name|void
+name|sql2Measure
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|test
+argument_list|(
+literal|"sql2_measure.txt"
+argument_list|)
+expr_stmt|;
+block|}
+annotation|@
+name|Test
+specifier|public
+name|void
+name|descendantTest
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|JsopUtil
+operator|.
+name|apply
+argument_list|(
+name|root
+argument_list|,
+literal|"/ + \"test\": { \"a\": {}, \"b\": {} }"
+argument_list|)
+expr_stmt|;
+name|root
+operator|.
+name|commit
+argument_list|()
+expr_stmt|;
+name|Iterator
+argument_list|<
+name|String
+argument_list|>
+name|result
+init|=
+name|executeQuery
+argument_list|(
+literal|"select * from [nt:base] where isdescendantnode('/test')"
+argument_list|,
+literal|"JCR-SQL2"
+argument_list|)
+operator|.
+name|iterator
+argument_list|()
+decl_stmt|;
+name|assertTrue
+argument_list|(
+name|result
+operator|.
+name|hasNext
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"/test/a"
+argument_list|,
+name|result
+operator|.
+name|next
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"/test/b"
+argument_list|,
+name|result
+operator|.
+name|next
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertFalse
+argument_list|(
+name|result
+operator|.
+name|hasNext
+argument_list|()
+argument_list|)
+expr_stmt|;
+block|}
+annotation|@
+name|Test
+specifier|public
+name|void
+name|descendantTest2
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|JsopUtil
+operator|.
+name|apply
+argument_list|(
+name|root
+argument_list|,
+literal|"/ + \"test\": { \"a\": { \"name\": [\"Hello\", \"World\" ] }, \"b\": { \"name\" : \"Hello\" }}"
+argument_list|)
+expr_stmt|;
+name|root
+operator|.
+name|commit
+argument_list|()
+expr_stmt|;
+name|Iterator
+argument_list|<
+name|String
+argument_list|>
+name|result
+init|=
+name|executeQuery
+argument_list|(
+literal|"select * from [nt:base] where isdescendantnode('/test') and name='World'"
+argument_list|,
+literal|"JCR-SQL2"
+argument_list|)
+operator|.
+name|iterator
+argument_list|()
+decl_stmt|;
+name|assertTrue
+argument_list|(
+name|result
+operator|.
+name|hasNext
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"/test/a"
+argument_list|,
+name|result
+operator|.
+name|next
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertFalse
+argument_list|(
+name|result
+operator|.
+name|hasNext
+argument_list|()
+argument_list|)
+expr_stmt|;
+block|}
+annotation|@
+name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"OAK-420"
+argument_list|)
+specifier|public
+name|void
+name|ischildnodeTest
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|JsopUtil
+operator|.
+name|apply
+argument_list|(
+name|root
+argument_list|,
+literal|"/ + \"parents\": { \"p0\": {\"id\": \"0\"}, \"p1\": {\"id\": \"1\"}, \"p2\": {\"id\": \"2\"}}"
+argument_list|)
+expr_stmt|;
+name|JsopUtil
+operator|.
+name|apply
+argument_list|(
+name|root
+argument_list|,
+literal|"/ + \"children\": { \"c1\": {\"p\": \"1\"}, \"c2\": {\"p\": \"1\"}, \"c3\": {\"p\": \"2\"}, \"c4\": {\"p\": \"3\"}}"
+argument_list|)
+expr_stmt|;
+name|root
+operator|.
+name|commit
+argument_list|()
+expr_stmt|;
+name|Iterator
+argument_list|<
+name|String
+argument_list|>
+name|result
+init|=
+name|executeQuery
+argument_list|(
+literal|"select * from [nt:base] as p inner join [nt:base] as p2 on ischildnode(p2, p) where p.[jcr:path] = '/'"
+argument_list|,
+literal|"JCR-SQL2"
+argument_list|)
+operator|.
+name|iterator
+argument_list|()
+decl_stmt|;
+name|assertTrue
+argument_list|(
+name|result
+operator|.
+name|hasNext
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"/, /children"
+argument_list|,
+name|result
+operator|.
+name|next
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"/, /jcr:system"
+argument_list|,
+name|result
+operator|.
+name|next
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"/, /oak:index"
+argument_list|,
+name|result
+operator|.
+name|next
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"/, /parents"
+argument_list|,
+name|result
+operator|.
+name|next
+argument_list|()
+argument_list|)
+expr_stmt|;
+name|assertFalse
+argument_list|(
+name|result
+operator|.
+name|hasNext
+argument_list|()
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+end_class
 
 end_unit
 
