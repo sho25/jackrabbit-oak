4ffe020e0bb101f5f05762abca0e6cb266fd8e34
Angela Schreiber <angela@apache.org>
2013-01-22 09:08:54 +0000

    javadoc (duplicate @return)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1436811 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 27150b09f977b5d0118cd8247d01e3cecf3b0eae

--- oak-mk/src/main/java/org/apache/jackrabbit/mk/store/RevisionStore.java
+++ oak-mk/src/main/java/org/apache/jackrabbit/mk/store/RevisionStore.java
@@ -139,7 +139,7 @@ name|void
 name|lockHead
 parameter_list|()
 function_decl|;
-comment|/**      * Put a new head commit. Must be called while holding a lock on the head.      *       * @param token      *            put token      * @param commit      *            commit      * @param branchRootId      *            former branch root id, if this is a merge; otherwise      *            {@code null}      * @return branchRevId      *            current branch head, i.e. last commit on this branch,       *            if this is a merge; otherwise {@code null}      * @return head commit id      * @throws Exception      *             if an error occurs      * @see #lockHead()      */
+comment|/**      * Put a new head commit. Must be called while holding a lock on the head.      *      * @param token      *            put token      * @param commit      *            commit      * @param branchRootId      *            former branch root id, if this is a merge; otherwise      *            {@code null}      * @param branchRevId      *            current branch head, i.e. last commit on this branch,       *            if this is a merge; otherwise {@code null}      * @return head commit id      * @throws Exception      *             if an error occurs      * @see #lockHead()      */
 name|Id
 comment|/*id*/
 name|putHeadCommit
