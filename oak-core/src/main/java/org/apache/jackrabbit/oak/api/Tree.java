8b52659f89fd9ca7abc320295e96ac7faf831013
Angela Schreiber <angela@apache.org>
2013-02-15 08:32:39 +0000

    OAK-626 : Allow for setting orderable child nodes on the parent tree
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1446474 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ecbf98bb9d179a43b9d1aa57ab3e854b26688430

--- oak-core/src/main/java/org/apache/jackrabbit/oak/api/Tree.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/api/Tree.java
@@ -38,7 +38,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A tree instance represents a snapshot of the {@code ContentRepository}  * tree at the time the instance was acquired. Tree instances may  * become invalid over time due to garbage collection of old content, at  * which point an outdated snapshot will start throwing  * {@code IllegalStateException}s to indicate that the snapshot is no  * longer available.  *<p/>  * The children of a {@code Tree} are generally unordered. That is, the  * sequence of the children returned by {@link #getChildren()} may change over  * time as this Tree is modified either directly or through some other session.  * Calling {@link #orderBefore(String)} will persist the current order and  * maintain the order as new children are added or removed. In this case a new  * child will be inserted after the last child as seen by {@link #getChildren()}.  *<p>  * A tree instance belongs to the client and its state is only modified  * in response to method calls made by the client. The various accessors  * on this interface mirror these of the underlying {@code NodeState}  * interface. However, since instances of this class are mutable return  * values may change between invocations.  *<p>  * Tree instances are not thread-safe for write access, so writing clients  * need to ensure that they are not accessed concurrently from multiple  * threads. Instances are however thread-safe for read access, so  * implementations need to ensure that all reading clients see a  * coherent state.  *<p>  * The data returned by this class and intermediary objects such as  * {@link PropertyState} is filtered for the access rights that are set in the  * {@link ContentSession} that created the {@link Root} of this object.  *<p>  * All tree instances created in the context of a content session become invalid  * after the content session is closed. Any method called on an invalid tree instance  * will throw an {@code InvalidStateException}.  */
+comment|/**  * A tree instance represents a snapshot of the {@code ContentRepository}  * tree at the time the instance was acquired. Tree instances may  * become invalid over time due to garbage collection of old content, at  * which point an outdated snapshot will start throwing  * {@code IllegalStateException}s to indicate that the snapshot is no  * longer available.  *<p/>  * The children of a {@code Tree} are generally unordered. That is, the  * sequence of the children returned by {@link #getChildren()} may change over  * time as this Tree is modified either directly or through some other session.  * Calling {@link #orderBefore(String)} will persist the current order and  * maintain the order as new children are added or removed. In this case a new  * child will be inserted after the last child as seen by {@link #getChildren()}.  *<p/>  * A tree instance belongs to the client and its state is only modified  * in response to method calls made by the client. The various accessors  * on this interface mirror these of the underlying {@code NodeState}  * interface. However, since instances of this class are mutable return  * values may change between invocations.  *<p/>  * Tree instances are not thread-safe for write access, so writing clients  * need to ensure that they are not accessed concurrently from multiple  * threads. Instances are however thread-safe for read access, so  * implementations need to ensure that all reading clients see a  * coherent state.  *<p/>  * The data returned by this class and intermediary objects such as  * {@link PropertyState} is filtered for the access rights that are set in the  * {@link ContentSession} that created the {@link Root} of this object.  *<p/>  * All tree instances created in the context of a content session become invalid  * after the content session is closed. Any method called on an invalid tree instance  * will throw an {@code InvalidStateException}.  */
 end_comment
 
 begin_interface
@@ -62,7 +62,7 @@ block|,
 comment|/**          * Item is removed or has become disconnected otherwise (e.g. caused by a refresh).          */
 name|DISCONNECTED
 block|}
-comment|/**      * @return  the name of this {@code Tree} instance.      */
+comment|/**      * @return the name of this {@code Tree} instance.      */
 annotation|@
 name|Nonnull
 name|String
@@ -95,14 +95,14 @@ name|TreeLocation
 name|getLocation
 parameter_list|()
 function_decl|;
-comment|/**      * @return the parent of this {@code Tree} instance. This method returns      * {@code null} if the parent is not accessible or if no parent exists (root      * node).      */
+comment|/**      * @return the parent of this {@code Tree} instance. This method returns      *         {@code null} if the parent is not accessible or if no parent exists (root      *         node).      */
 annotation|@
 name|CheckForNull
 name|Tree
 name|getParent
 parameter_list|()
 function_decl|;
-comment|/**      * Get a property state      *      * @param name The name of the property state.      * @return the property state with the given {@code name} or {@code null}      * if no such property state exists or the property is not accessible.      */
+comment|/**      * Get a property state      *      * @param name The name of the property state.      * @return the property state with the given {@code name} or {@code null}      *         if no such property state exists or the property is not accessible.      */
 annotation|@
 name|CheckForNull
 name|PropertyState
@@ -112,7 +112,7 @@ name|String
 name|name
 parameter_list|)
 function_decl|;
-comment|/**      * Get the {@code Status} of a property state or {@code null}.      *      * @param name The name of the property state.      * @return The status of the property state with the given {@code name}      * or {@code null} in no such property state exists or if the name refers      * to a property that is not accessible.      */
+comment|/**      * Get the {@code Status} of a property state or {@code null}.      *      * @param name The name of the property state.      * @return The status of the property state with the given {@code name}      *         or {@code null} in no such property state exists or if the name refers      *         to a property that is not accessible.      */
 annotation|@
 name|CheckForNull
 name|Status
@@ -122,7 +122,7 @@ name|String
 name|name
 parameter_list|)
 function_decl|;
-comment|/**      * Determine if a property state exists and is accessible.      *      * @param name The name of the property state      * @return {@code true} if and only if a property with the given {@code name}      *          exists and is accessible.      */
+comment|/**      * Determine if a property state exists and is accessible.      *      * @param name The name of the property state      * @return {@code true} if and only if a property with the given {@code name}      *         exists and is accessible.      */
 name|boolean
 name|hasProperty
 parameter_list|(
@@ -147,7 +147,7 @@ argument_list|>
 name|getProperties
 parameter_list|()
 function_decl|;
-comment|/**      * Get a child of this {@code Tree} instance.      *      * @param name The name of the child to retrieve.      * @return The child with the given {@code name} or {@code null} if no such      * child exists or the child is not accessible.      */
+comment|/**      * Get a child of this {@code Tree} instance.      *      * @param name The name of the child to retrieve.      * @return The child with the given {@code name} or {@code null} if no such      *         child exists or the child is not accessible.      */
 annotation|@
 name|CheckForNull
 name|Tree
@@ -157,7 +157,7 @@ name|String
 name|name
 parameter_list|)
 function_decl|;
-comment|/**      * Determine if a child of this {@code Tree} instance exists. If no child      * exists or an existing child isn't accessible this method returns {@code false}.      *      * @param name The name of the child      * @return {@code true} if and only if a child with the given {@code name}      * exists and is accessible for the current content session.      */
+comment|/**      * Determine if a child of this {@code Tree} instance exists. If no child      * exists or an existing child isn't accessible this method returns {@code false}.      *      * @param name The name of the child      * @return {@code true} if and only if a child with the given {@code name}      *         exists and is accessible for the current content session.      */
 name|boolean
 name|hasChild
 parameter_list|(
@@ -195,6 +195,14 @@ name|String
 name|name
 parameter_list|)
 function_decl|;
+comment|/**      * Changes the nature of this tree such that the order of the children      * is kept stable. The expected behavior is as follows:      *<p/>      *<ol>      *<li>Calling {@code setOrderableChildren(true)} on a tree      * the first time will stabilize the order of existing children. Any      * subsequent {@link #addChild(String)} call is guaranteed to insert      * the new tree and the end of the child list.</li>      *<li>Calling {@code setOrderableChildren(true)} on a tree      * that already has its children ordered has no effect.</li>      *<li>Calling {@code setOrderableChildren(false)} on a tree that      * doesn't have ordered children has not effect</li>      *<li>Calling {@code setOrderableChildren(false)} on a tree      * with ordered children will remove the necessity to keep the child      * list stable. The order of children upon {@link #getChildren()} is      * subsequently undefined.</li>      *</ol>      *<p/>      * Calling {@link #orderBefore(String)} on a tree, implicitly enables      * orderable children on the parent tree.      *      * @param enable Enable (or disable) orderable children for this tree.      */
+name|void
+name|setOrderableChildren
+parameter_list|(
+name|boolean
+name|enable
+parameter_list|)
+function_decl|;
 comment|/**      * Orders this {@code Tree} before the sibling tree with the given      * {@code name}. Calling this method for the first time on this      * {@code Tree} or any of its siblings will persist the current order      * of siblings and maintain it from this point on.      *      * @param name the name of the sibling node where this tree is ordered      *             before. This tree will become the last sibling if      *             {@code name} is {@code null}.      * @return {@code false} if there is no sibling with the given      *         {@code name} and no reordering was performed;      *         {@code true} otherwise.      */
 name|boolean
 name|orderBefore
@@ -203,7 +211,7 @@ name|String
 name|name
 parameter_list|)
 function_decl|;
-comment|/**      * Set a property state      * @param property  The property state to set      */
+comment|/**      * Set a property state      *      * @param property The property state to set      */
 name|void
 name|setProperty
 parameter_list|(
@@ -213,7 +221,7 @@ name|PropertyState
 name|property
 parameter_list|)
 function_decl|;
-comment|/**      * Set a property state      * @param name  The name of this property      * @param value  The value of this property      * @param<T>  The type of this property. Must be one of {@code String, Blob, byte[], Long, Integer, Double, Boolean, BigDecimal}      * @throws IllegalArgumentException if {@code T} is not one of the above types.      */
+comment|/**      * Set a property state      *      * @param name  The name of this property      * @param value The value of this property      * @param<T>   The type of this property. Must be one of {@code String, Blob, byte[], Long, Integer, Double, Boolean, BigDecimal}      * @throws IllegalArgumentException if {@code T} is not one of the above types.      */
 parameter_list|<
 name|T
 parameter_list|>
@@ -229,7 +237,7 @@ name|T
 name|value
 parameter_list|)
 function_decl|;
-comment|/**      * Set a property state      * @param name  The name of this property      * @param value  The value of this property      * @param type The type of this property.      * @param<T>  The type of this property.      */
+comment|/**      * Set a property state      *      * @param name  The name of this property      * @param value The value of this property      * @param type  The type of this property.      * @param<T>   The type of this property.      */
 parameter_list|<
 name|T
 parameter_list|>
