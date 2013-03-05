187b8e09cc2470dba747c95ccddb52bb8aebb7ae
Jukka Zitting <jukka@apache.org>
2013-03-05 14:35:10 +0000

    OAK-593: Segment-based MK
    Unused imports
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1452809 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8e3fa7c01d725087cf5524d69feb85e6c7d46d84

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentBlob.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentBlob.java
@@ -37,26 +37,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|security
-operator|.
-name|MessageDigest
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|security
-operator|.
-name|NoSuchAlgorithmException
-import|;
-end_import
-
-begin_import
-import|import
 name|javax
 operator|.
 name|annotation
