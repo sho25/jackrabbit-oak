1bd56ee7050580913e27a6bf68297732d76f01b0
Alexandru Parvulescu <alexparvulescu@apache.org>
2013-04-25 14:31:56 +0000

    OAK-734 Refactor indexing code to use Editors
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1475797 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8e7c6d2b60f67c4b7c8a135bc066b460ec3c28a9

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/Main.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/Main.java
@@ -219,9 +219,9 @@ name|plugins
 operator|.
 name|index
 operator|.
-name|p2
+name|property
 operator|.
-name|Property2IndexHookProvider
+name|PropertyIndexEditorProvider
 import|;
 end_import
 
@@ -1128,7 +1128,7 @@ operator|.
 name|with
 argument_list|(
 operator|new
-name|Property2IndexHookProvider
+name|PropertyIndexEditorProvider
 argument_list|()
 argument_list|)
 operator|.
