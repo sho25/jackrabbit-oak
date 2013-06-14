7f2320081eb7dfe1bd73286808297a7c8fdf3bbf
Jukka Zitting <jukka@apache.org>
2013-06-14 08:20:37 +0000

    unused import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1492993 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bf641b4d385ae03ddb2cc9434143d741402799b0

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/observation/ObservationManagerImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/observation/ObservationManagerImpl.java
@@ -163,20 +163,6 @@ end_import
 
 begin_import
 import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|collect
-operator|.
-name|Lists
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
