0f184e8aa39470473cfdc1f498eb3129123685ba
Jukka Zitting <jukka@apache.org>
2013-07-16 07:46:11 +0000

    OAK-804: MBean to track observation listeners
    The ObjectName component values need to be quoted
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1503593 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bf2496b9ed99ae47ebddb9fe63216d201fdaee4f

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/whiteboard/WhiteboardUtils.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/whiteboard/WhiteboardUtils.java
@@ -194,7 +194,12 @@ name|put
 argument_list|(
 literal|"type"
 argument_list|,
+name|ObjectName
+operator|.
+name|quote
+argument_list|(
 name|type
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|table
@@ -203,7 +208,12 @@ name|put
 argument_list|(
 literal|"name"
 argument_list|,
+name|ObjectName
+operator|.
+name|quote
+argument_list|(
 name|name
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|table
