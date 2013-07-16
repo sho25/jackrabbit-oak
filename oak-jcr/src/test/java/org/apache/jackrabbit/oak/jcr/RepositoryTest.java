2627a077d41bf8b08efe23f2649e0395c4ed8192
Michael Dürig <mduerig@apache.org>
2013-07-16 15:07:00 +0000

    OAK-803 Backwards compatibility of long-lived sessions make admin sessions refresh on every access
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1503748 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e1ce0e0a8b662433b76701eb1d5ad44c7a23b169

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
@@ -8513,55 +8513,7 @@ operator|.
 name|save
 argument_list|()
 expr_stmt|;
-comment|// Make sure they are still not accessible through another session
-name|assertFalse
-argument_list|(
-name|session2
-operator|.
-name|itemExists
-argument_list|(
-literal|"/node1"
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|assertFalse
-argument_list|(
-name|session2
-operator|.
-name|itemExists
-argument_list|(
-literal|"/node1/node2"
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|assertFalse
-argument_list|(
-name|session2
-operator|.
-name|itemExists
-argument_list|(
-literal|"/node1/node3"
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|assertFalse
-argument_list|(
-name|session2
-operator|.
-name|itemExists
-argument_list|(
-literal|"/node1/node3/property1"
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|session2
-operator|.
-name|refresh
-argument_list|(
-literal|false
-argument_list|)
-expr_stmt|;
-comment|// Make sure they are accessible through another session after refresh
+comment|// Make sure these items are still accessible through another session
 name|assertTrue
 argument_list|(
 name|session2
@@ -9184,7 +9136,7 @@ literal|"node1"
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|assertFalse
+name|assertTrue
 argument_list|(
 name|session1
 operator|.
@@ -9197,7 +9149,6 @@ literal|"node2"
 argument_list|)
 argument_list|)
 expr_stmt|;
-comment|// was not visible during save
 name|assertTrue
 argument_list|(
 name|session2
@@ -9211,7 +9162,6 @@ literal|"node1"
 argument_list|)
 argument_list|)
 expr_stmt|;
-comment|// save refreshes
 name|assertTrue
 argument_list|(
 name|session2
@@ -9538,7 +9488,7 @@ literal|"node"
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|assertTrue
+name|assertFalse
 argument_list|(
 name|session2
 operator|.
