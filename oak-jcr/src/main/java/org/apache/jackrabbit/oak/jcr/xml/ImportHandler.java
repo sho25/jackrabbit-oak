66d6196d54b2028bd536699a4c6c252024090b3a
Chetan Mehrotra <chetanm@apache.org>
2013-09-25 14:38:47 +0000

    OAK-1037 - Improve performance of XmlImport
    Import logic perform check if a node with given uuid already exist or not. This check is costly
     if performed against a tree which has lots of transient changes. To speed up the process the logic
     is now broken into two part
    
    1. Look for a node with given id using an unmodified root
    2. Also make note of uuid of trees newly created and look into that
    
    This improves the import performance by factor of 10.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1526193 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f3d836ac19c4edacf7a15df5a0caa266a092bb8f

--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/xml/ImportHandler.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/xml/ImportHandler.java
@@ -292,6 +292,11 @@ name|root
 decl_stmt|;
 specifier|private
 specifier|final
+name|Root
+name|initialRoot
+decl_stmt|;
+specifier|private
+specifier|final
 name|SessionContext
 name|sessionContext
 decl_stmt|;
@@ -375,6 +380,16 @@ operator|.
 name|getSessionDelegate
 argument_list|()
 decl_stmt|;
+name|initialRoot
+operator|=
+name|sd
+operator|.
+name|getContentSession
+argument_list|()
+operator|.
+name|getLatestRoot
+argument_list|()
+expr_stmt|;
 name|root
 operator|=
 operator|(
@@ -415,6 +430,8 @@ argument_list|,
 name|sessionContext
 argument_list|,
 name|root
+argument_list|,
+name|initialRoot
 argument_list|,
 name|uuidBehavior
 argument_list|,
