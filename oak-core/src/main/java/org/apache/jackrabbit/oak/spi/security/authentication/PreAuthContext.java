d9d382e5d7e9c3e1da4f41b9a8bc19b20153de86
Angela Schreiber <angela@apache.org>
2013-10-16 18:22:20 +0000

    remove unused logger
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1532843 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b0df774f8cd0d6ad9e604c522cd45cdd1234d203

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/PreAuthContext.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/PreAuthContext.java
@@ -33,26 +33,6 @@ name|Subject
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|slf4j
-operator|.
-name|Logger
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|slf4j
-operator|.
-name|LoggerFactory
-import|;
-end_import
-
 begin_comment
 comment|/**  * LoginContext for pre-authenticated subjects that don't require further  * validation nor additional login/logout steps.  */
 end_comment
@@ -65,22 +45,6 @@ name|PreAuthContext
 implements|implements
 name|LoginContext
 block|{
-comment|/**      * logger instance      */
-specifier|private
-specifier|static
-specifier|final
-name|Logger
-name|log
-init|=
-name|LoggerFactory
-operator|.
-name|getLogger
-argument_list|(
-name|PreAuthContext
-operator|.
-name|class
-argument_list|)
-decl_stmt|;
 specifier|private
 specifier|final
 name|Subject
