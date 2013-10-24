448b507b7bac0c8dad521c17fefb73eb42d46c87
Angela Schreiber <angela@apache.org>
2013-10-24 15:03:50 +0000

    OAK-527: permissions (wip)
    - remove unused import
    - javadoc
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1535400 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cd311cb52266b8f4089e35971622d2ebbc0d5865

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/permission/EntryPredicate.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/permission/EntryPredicate.java
@@ -87,28 +87,8 @@ name|Tree
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|slf4j
-operator|.
-name|Logger
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|slf4j
-operator|.
-name|LoggerFactory
-import|;
-end_import
-
 begin_comment
-comment|/**  * EntryPredicate... TODO  */
+comment|/**  * Predicate used to evaluation if a given {@code PermissionEntry} matches  * the specified tree, property or path.  */
 end_comment
 
 begin_class
