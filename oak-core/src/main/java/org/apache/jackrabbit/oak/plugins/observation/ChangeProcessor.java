bf4cfb833c978bf775e148d324d8d9cba05e136a
Michael Dürig <mduerig@apache.org>
2013-10-30 08:36:03 +0000

    OAK-144 Implement observation Set thread's interrupted state on InterruptedException
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1537008 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 003cce1a2e1946edf1ea3d5c03d047060cc9e4b5

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/observation/ChangeProcessor.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/observation/ChangeProcessor.java
@@ -576,6 +576,14 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
+name|Thread
+operator|.
+name|currentThread
+argument_list|()
+operator|.
+name|interrupt
+argument_list|()
+expr_stmt|;
 block|}
 block|}
 block|}
