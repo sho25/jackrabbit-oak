d15647c3a7f9b01c35f728a16016041098a412dc
Tobias Bocanegra <tripod@apache.org>
2014-01-10 00:42:26 +0000

    OAK-1317 OutOfMemoryError in org.apache.jackrabbit.mk.json.JsopBuilder.toString
    - disabling test case
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1556997 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cc907a9f1cc24c0eaa5da885b57380a178a0d306

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/UserImportTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/UserImportTest.java
@@ -277,6 +277,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -358,6 +368,11 @@ comment|/**  * Testing user import with default {@link org.apache.jackrabbit.oak
 end_comment
 
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Produces OOME. see OAK-1317"
+argument_list|)
 specifier|public
 class|class
 name|UserImportTest
