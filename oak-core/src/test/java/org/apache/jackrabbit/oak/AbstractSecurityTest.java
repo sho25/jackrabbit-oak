2f3e90bac56338b021467a7d784375604ac31c84
Tobias Bocanegra <tripod@apache.org>
2014-02-11 00:19:23 +0000

    OAK-516 Create LdapLoginModule based on ExternalLoginModule
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1566888 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9f8b4402344e5d799868bb546e0b4b25d788e54e

--- oak-core/src/test/java/org/apache/jackrabbit/oak/AbstractSecurityTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/AbstractSecurityTest.java
@@ -16,22 +16,6 @@ package|;
 end_package
 
 begin_import
-import|import static
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|collect
-operator|.
-name|Lists
-operator|.
-name|newArrayList
-import|;
-end_import
-
-begin_import
 import|import
 name|java
 operator|.
@@ -670,6 +654,22 @@ operator|.
 name|junit
 operator|.
 name|Before
+import|;
+end_import
+
+begin_import
+import|import static
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|collect
+operator|.
+name|Lists
+operator|.
+name|newArrayList
 import|;
 end_import
 
