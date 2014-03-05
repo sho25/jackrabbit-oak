1a2da451731897d030e6d080d1d4587a874b8729
Thomas Mueller <thomasm@apache.org>
2014-03-05 13:58:19 +0000

    OAK-622 Improve QueryIndex interface (WIP)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1574491 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d20f8386337c24b14c55c488889c6e43685c1b88

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/query/QueryIndex.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/query/QueryIndex.java
@@ -21,11 +21,47 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|ArrayList
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
+import|;
+end_import
+
+begin_import
+import|import
 name|javax
 operator|.
 name|annotation
 operator|.
 name|CheckForNull
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|api
+operator|.
+name|Type
 import|;
 end_import
 
@@ -129,105 +165,606 @@ name|getNodeAggregator
 parameter_list|()
 function_decl|;
 block|}
-comment|//    /**
-comment|//     * Return the possible index plans for the given filter and sort order.
-comment|//     * Please note this method is supposed to run quickly. That means it should
-comment|//     * usually not read any data from the storage.
-comment|//     *
-comment|//     * @param filter the filter
-comment|//     * @param sortOrder the sort order or null if no sorting is required
-comment|//     * @param rootState root state of the current repository snapshot
-comment|//     * @return the list of index plans (null if none)
-comment|//     */
-comment|//    List<IndexPlan> getPlans(Filter filter, List<Order> sortOrder, NodeState rootState);
-comment|//
-comment|//    /**
-comment|//     * Get the query plan description (for logging purposes).
-comment|//     *
-comment|//     * @param plan the index plan
-comment|//     * @return the query plan description
-comment|//     */
-comment|//    String getPlanDescription(IndexPlan plan);
-comment|//
-comment|//    /**
-comment|//     * Start a query. The filter and sort order of the index plan is to be used.
-comment|//     *
-comment|//     * @param plan the index plan to use
-comment|//     * @param rootState root state of the current repository snapshot
-comment|//     * @return a cursor to iterate over the result
-comment|//     */
-comment|//    Cursor query(IndexPlan plan, NodeState rootState);
-comment|//
-comment|//    /**
-comment|//     * An index plan.
-comment|//     */
-comment|//    public static class IndexPlan {
-comment|//
-comment|//        /**
-comment|//         * The cost to execute the query once. The returned value should
-comment|//         * approximately match the number of disk read operations plus the
-comment|//         * number of network roundtrips (worst case).
-comment|//         */
-comment|//        double costPerExecution;
-comment|//
-comment|//        /**
-comment|//         * The cost to read one entry from the cursor. The returned value should
-comment|//         * approximately match the number of disk read operations plus the
-comment|//         * number of network roundtrips (worst case).
-comment|//         */
-comment|//        double costPerEntry;
-comment|//
-comment|//        /**
-comment|//         * The estimated number of entries. This value does not have to be
-comment|//         * accurate.
-comment|//         */
-comment|//        long estimatedEntryCount;
-comment|//
-comment|//        /**
-comment|//         * The filter to use.
-comment|//         */
-comment|//        Filter filter;
-comment|//
-comment|//        /**
-comment|//         * Whether the index is not always up-to-date.
-comment|//         */
-comment|//        boolean isDelayed;
-comment|//
-comment|//        /**
-comment|//         * Whether the fulltext part of the filter is evaluated (possibly with
-comment|//         * an extended syntax). If set, the fulltext part of the filter is not
-comment|//         * evaluated any more within the query engine.
-comment|//         */
-comment|//        boolean isFulltextIndex;
-comment|//
-comment|//        /**
-comment|//         * Whether the cursor is able to read all properties from a node.
-comment|//         */
-comment|//        boolean includesNodeData;
-comment|//
-comment|//        /**
-comment|//         * The sort order of the returned entries, or null if unsorted.
-comment|//         */
-comment|//        List<Order> sortOrder;
-comment|//
-comment|//    }
-comment|//
-comment|//    /**
-comment|//     * A sort order entry.
-comment|//     */
-comment|//    static class Order {
-comment|//
-comment|//        /**
-comment|//         * The property name on where to sort.
-comment|//         */
-comment|//        String propertyName;
-comment|//
-comment|//        /**
-comment|//         * True for descending, false for ascending.
-comment|//         */
-comment|//        boolean descending;
-comment|//
-comment|//    }
+comment|/**      * An query index that may support using multiple access orders      * (returning the rows in a specific order), and that can provide detailed      * information about the cost.      */
+specifier|public
+interface|interface
+name|AdvancedQueryIndex
+block|{
+comment|/**          * Return the possible index plans for the given filter and sort order.          * Please note this method is supposed to run quickly. That means it          * should usually not read any data from the storage.          *           * @param filter the filter          * @param sortOrder the sort order or null if no sorting is required          * @param rootState root state of the current repository snapshot          * @return the list of index plans (null if none)          */
+name|List
+argument_list|<
+name|IndexPlan
+argument_list|>
+name|getPlans
+parameter_list|(
+name|Filter
+name|filter
+parameter_list|,
+name|List
+argument_list|<
+name|OrderEntry
+argument_list|>
+name|sortOrder
+parameter_list|,
+name|NodeState
+name|rootState
+parameter_list|)
+function_decl|;
+comment|/**          * Get the query plan description (for logging purposes).          *           * @param plan the index plan          * @return the query plan description          */
+name|String
+name|getPlanDescription
+parameter_list|(
+name|IndexPlan
+name|plan
+parameter_list|)
+function_decl|;
+comment|/**          * Start a query. The filter and sort order of the index plan is to be          * used.          *           * @param plan the index plan to use          * @param rootState root state of the current repository snapshot          * @return a cursor to iterate over the result          */
+name|Cursor
+name|query
+parameter_list|(
+name|IndexPlan
+name|plan
+parameter_list|,
+name|NodeState
+name|rootState
+parameter_list|)
+function_decl|;
+block|}
+comment|/**      * An index plan.      */
+specifier|public
+interface|interface
+name|IndexPlan
+block|{
+comment|/**          * The cost to execute the query once. The returned value should          * approximately match the number of disk read operations plus the          * number of network roundtrips (worst case).          *           * @return the cost per execution, in estimated number of I/O operations          */
+name|double
+name|getCostPerExecution
+parameter_list|()
+function_decl|;
+comment|/**          * The cost to read one entry from the cursor. The returned value should          * approximately match the number of disk read operations plus the          * number of network roundtrips (worst case).          *           * @return the lookup cost per entry, in estimated number of I/O operations          */
+name|double
+name|getCostPerEntry
+parameter_list|()
+function_decl|;
+comment|/**          * The estimated number of entries. This value does not have to be          * accurate.          *           * @return the estimated number of entries          */
+name|long
+name|getEstimatedEntryCount
+parameter_list|()
+function_decl|;
+comment|/**          * The filter to use.          *           * @return the filter          */
+name|Filter
+name|getFilter
+parameter_list|()
+function_decl|;
+comment|/**          * Whether the index is not always up-to-date.          *           * @return whether the index might be updated asynchronously          */
+name|boolean
+name|isDelayed
+parameter_list|()
+function_decl|;
+comment|/**          * Whether the fulltext part of the filter is evaluated (possibly with          * an extended syntax). If set, the fulltext part of the filter is not          * evaluated any more within the query engine.          *           * @return whether the index supports full-text extraction          */
+name|boolean
+name|isFulltextIndex
+parameter_list|()
+function_decl|;
+comment|/**          * Whether the cursor is able to read all properties from a node.          * If yes, then the query engine will not have to read the data itself.          *           * @return wheter node data is returned          */
+name|boolean
+name|includesNodeData
+parameter_list|()
+function_decl|;
+comment|/**          * The sort order of the returned entries, or null if unsorted.          *           * @return the sort order          */
+name|List
+argument_list|<
+name|OrderEntry
+argument_list|>
+name|getSortOrder
+parameter_list|()
+function_decl|;
+comment|/**          * A builder for index plans.          */
+specifier|public
+class|class
+name|Builder
+block|{
+specifier|protected
+name|double
+name|costPerExecution
+init|=
+literal|1.0
+decl_stmt|;
+specifier|protected
+name|double
+name|costPerEntry
+init|=
+literal|1.0
+decl_stmt|;
+specifier|protected
+name|long
+name|estimatedEntryCount
+init|=
+literal|1000000
+decl_stmt|;
+specifier|protected
+name|Filter
+name|filter
+decl_stmt|;
+specifier|protected
+name|boolean
+name|isDelayed
+decl_stmt|;
+specifier|protected
+name|boolean
+name|isFulltextIndex
+decl_stmt|;
+specifier|protected
+name|boolean
+name|includesNodeData
+decl_stmt|;
+specifier|protected
+name|List
+argument_list|<
+name|OrderEntry
+argument_list|>
+name|sortOrder
+decl_stmt|;
+specifier|public
+name|Builder
+name|setCostPerExecution
+parameter_list|(
+name|double
+name|costPerExecution
+parameter_list|)
+block|{
+name|this
+operator|.
+name|costPerExecution
+operator|=
+name|costPerExecution
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|Builder
+name|setCostPerEntry
+parameter_list|(
+name|double
+name|costPerEntry
+parameter_list|)
+block|{
+name|this
+operator|.
+name|costPerEntry
+operator|=
+name|costPerEntry
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|Builder
+name|setEstimatedEntryCount
+parameter_list|(
+name|long
+name|estimatedEntryCount
+parameter_list|)
+block|{
+name|this
+operator|.
+name|estimatedEntryCount
+operator|=
+name|estimatedEntryCount
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|Builder
+name|setFilter
+parameter_list|(
+name|Filter
+name|filter
+parameter_list|)
+block|{
+name|this
+operator|.
+name|filter
+operator|=
+name|filter
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|Builder
+name|setDelayed
+parameter_list|(
+name|boolean
+name|isDelayed
+parameter_list|)
+block|{
+name|this
+operator|.
+name|isDelayed
+operator|=
+name|isDelayed
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|Builder
+name|setFulltextIndex
+parameter_list|(
+name|boolean
+name|isFulltextIndex
+parameter_list|)
+block|{
+name|this
+operator|.
+name|isFulltextIndex
+operator|=
+name|isFulltextIndex
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|Builder
+name|setIncludesNodeData
+parameter_list|(
+name|boolean
+name|includesNodeData
+parameter_list|)
+block|{
+name|this
+operator|.
+name|includesNodeData
+operator|=
+name|includesNodeData
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|Builder
+name|setSortOrder
+parameter_list|(
+name|List
+argument_list|<
+name|OrderEntry
+argument_list|>
+name|sortOrder
+parameter_list|)
+block|{
+name|this
+operator|.
+name|sortOrder
+operator|=
+name|sortOrder
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+specifier|public
+name|IndexPlan
+name|build
+parameter_list|()
+block|{
+return|return
+operator|new
+name|IndexPlan
+argument_list|()
+block|{
+specifier|private
+specifier|final
+name|double
+name|costPerExecution
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|costPerExecution
+decl_stmt|;
+specifier|private
+specifier|final
+name|double
+name|costPerEntry
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|costPerEntry
+decl_stmt|;
+specifier|private
+specifier|final
+name|long
+name|estimatedEntryCount
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|estimatedEntryCount
+decl_stmt|;
+specifier|private
+specifier|final
+name|Filter
+name|filter
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|filter
+decl_stmt|;
+specifier|private
+specifier|final
+name|boolean
+name|isDelayed
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|isDelayed
+decl_stmt|;
+specifier|private
+specifier|final
+name|boolean
+name|isFulltextIndex
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|isFulltextIndex
+decl_stmt|;
+specifier|private
+specifier|final
+name|boolean
+name|includesNodeData
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|includesNodeData
+decl_stmt|;
+specifier|private
+specifier|final
+name|List
+argument_list|<
+name|OrderEntry
+argument_list|>
+name|sortOrder
+init|=
+name|Builder
+operator|.
+name|this
+operator|.
+name|sortOrder
+operator|==
+literal|null
+condition|?
+literal|null
+else|:
+operator|new
+name|ArrayList
+argument_list|<
+name|OrderEntry
+argument_list|>
+argument_list|(
+name|Builder
+operator|.
+name|this
+operator|.
+name|sortOrder
+argument_list|)
+decl_stmt|;
+annotation|@
+name|Override
+specifier|public
+name|double
+name|getCostPerExecution
+parameter_list|()
+block|{
+return|return
+name|costPerExecution
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|double
+name|getCostPerEntry
+parameter_list|()
+block|{
+return|return
+name|costPerEntry
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|long
+name|getEstimatedEntryCount
+parameter_list|()
+block|{
+return|return
+name|estimatedEntryCount
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|Filter
+name|getFilter
+parameter_list|()
+block|{
+return|return
+name|filter
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|isDelayed
+parameter_list|()
+block|{
+return|return
+name|isDelayed
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|isFulltextIndex
+parameter_list|()
+block|{
+return|return
+name|isFulltextIndex
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|includesNodeData
+parameter_list|()
+block|{
+return|return
+name|includesNodeData
+return|;
+block|}
+annotation|@
+name|Override
+specifier|public
+name|List
+argument_list|<
+name|OrderEntry
+argument_list|>
+name|getSortOrder
+parameter_list|()
+block|{
+return|return
+name|sortOrder
+return|;
+block|}
+block|}
+return|;
+block|}
+block|}
+block|}
+comment|/**      * A sort order entry.      */
+specifier|static
+class|class
+name|OrderEntry
+block|{
+comment|/**          * The property name on where to sort.          */
+specifier|private
+specifier|final
+name|String
+name|propertyName
+decl_stmt|;
+comment|/**          * The property type. Null if not known.          */
+specifier|private
+specifier|final
+name|Type
+argument_list|<
+name|?
+argument_list|>
+name|propertyType
+decl_stmt|;
+comment|/**          * The sort order (ascending or descending).          */
+specifier|public
+enum|enum
+name|Order
+block|{
+name|ASCENDING
+block|,
+name|DESCENDING
+block|}
+empty_stmt|;
+specifier|private
+specifier|final
+name|Order
+name|order
+decl_stmt|;
+name|OrderEntry
+parameter_list|(
+name|String
+name|propertyName
+parameter_list|,
+name|Type
+argument_list|<
+name|?
+argument_list|>
+name|propertyType
+parameter_list|,
+name|Order
+name|order
+parameter_list|)
+block|{
+name|this
+operator|.
+name|propertyName
+operator|=
+name|propertyName
+expr_stmt|;
+name|this
+operator|.
+name|propertyType
+operator|=
+name|propertyType
+expr_stmt|;
+name|this
+operator|.
+name|order
+operator|=
+name|order
+expr_stmt|;
+block|}
+specifier|public
+name|String
+name|getPropertyName
+parameter_list|()
+block|{
+return|return
+name|propertyName
+return|;
+block|}
+specifier|public
+name|Order
+name|getOrder
+parameter_list|()
+block|{
+return|return
+name|order
+return|;
+block|}
+specifier|public
+name|Type
+argument_list|<
+name|?
+argument_list|>
+name|getPropertyType
+parameter_list|()
+block|{
+return|return
+name|propertyType
+return|;
+block|}
+block|}
 block|}
 end_interface
 
