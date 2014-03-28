783d207e785cd0732bbf1e276f678e5aa15108bf
Michael Dürig <mduerig@apache.org>
2014-03-28 15:50:41 +0000

    OAK-1174 : Inconsistent handling of invalid names/paths typo
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1582783 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0c4c12dc0415d1d97e60ce3744f43fa3b2ce8a17

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/NamePathTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/NamePathTest.java
@@ -18,6 +18,30 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertNotNull
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertNull
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -108,30 +132,6 @@ operator|.
 name|slf4j
 operator|.
 name|LoggerFactory
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertNotNull
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertNull
 import|;
 end_import
 
@@ -452,7 +452,7 @@ annotation|@
 name|Test
 specifier|public
 name|void
-name|testAskeriskInName
+name|testAsteriskInName
 parameter_list|()
 throws|throws
 name|RepositoryException
