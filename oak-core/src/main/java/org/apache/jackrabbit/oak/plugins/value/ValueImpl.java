bfcee8f8537a468c5d3fe3baa695b32da1f6895a
Michael Dürig <mduerig@apache.org>
2014-03-31 19:23:22 +0000

    OAK-1489: ValueImpl should implement JackrabbitValue Expose the blob reference as the content identifier, 2nd try: use content has as identifier
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1583413 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2cafb391703e19f0b8a46225f79c41f0d4aefd7b

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/value/ValueImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/value/ValueImpl.java
@@ -52,6 +52,26 @@ import|;
 end_import
 
 begin_import
+import|import static
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|plugins
+operator|.
+name|memory
+operator|.
+name|AbstractBlob
+operator|.
+name|calculateSha256
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -1054,10 +1074,13 @@ name|getContentIdentity
 parameter_list|()
 block|{
 return|return
+name|calculateSha256
+argument_list|(
 name|getBlob
 argument_list|()
+argument_list|)
 operator|.
-name|getReference
+name|toString
 argument_list|()
 return|;
 block|}
