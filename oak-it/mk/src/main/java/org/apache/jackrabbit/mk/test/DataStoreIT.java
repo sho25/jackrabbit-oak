916e1fe28dc04dcb28ac5c713331afc93908789d
Michael Dürig <mduerig@apache.org>
2014-06-30 11:19:36 +0000

    OAK-1327: Cleanup NodeStore and MK implementations Move MicroKernelInputStream to oak-commons to reduce code duplication and dependencies on oak-mk
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1606688 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5dfee7623dfbe69778bf37437e6ab8d0ee7409f6

--- oak-it/mk/src/main/java/org/apache/jackrabbit/mk/test/DataStoreIT.java
+++ oak-it/mk/src/main/java/org/apache/jackrabbit/mk/test/DataStoreIT.java
@@ -85,9 +85,11 @@ name|apache
 operator|.
 name|jackrabbit
 operator|.
-name|mk
+name|oak
 operator|.
-name|util
+name|commons
+operator|.
+name|mk
 operator|.
 name|MicroKernelInputStream
 import|;
--- oak-it/mk/src/main/java/org/apache/jackrabbit/mk/test/MicroKernelIT.java
+++ oak-it/mk/src/main/java/org/apache/jackrabbit/mk/test/MicroKernelIT.java
@@ -211,9 +211,11 @@ name|apache
 operator|.
 name|jackrabbit
 operator|.
-name|mk
+name|oak
 operator|.
-name|util
+name|commons
+operator|.
+name|mk
 operator|.
 name|MicroKernelInputStream
 import|;
