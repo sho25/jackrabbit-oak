067a5f549242c32a38cc2cd1400814cc738090a4
Manfred Baedke <baedke@apache.org>
2014-09-19 10:10:08 +0000

    OAK-1915: TarMK Cold Standby
    Improved MBeans, added documentation.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1626168 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cb4e1724b74c0623f0f473031e6fcfd1f12443c3

--- oak-tarmk-failover/src/test/java/org/apache/jackrabbit/oak/plugins/segment/failover/BulkTest.java
+++ oak-tarmk-failover/src/test/java/org/apache/jackrabbit/oak/plugins/segment/failover/BulkTest.java
@@ -23,16 +23,6 @@ end_package
 
 begin_import
 import|import
-name|junit
-operator|.
-name|framework
-operator|.
-name|Assert
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
