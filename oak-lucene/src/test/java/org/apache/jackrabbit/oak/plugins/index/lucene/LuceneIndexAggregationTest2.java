180fa16549d9a832c06d8b7d6020b4978362349a
Thomas Mueller <thomasm@apache.org>
2014-10-29 15:25:27 +0000

    OAK-2226 Aggregate Lucene no results for multiple jcr:contains (remove unused import)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1635176 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 721fee88a5e89dbdefefd6500dcbd67b85ac16fc

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAggregationTest2.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAggregationTest2.java
@@ -599,16 +599,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
