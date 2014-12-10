7f9eeea94ac5c4561c693339c903ed7ed0bf99b2
Michael Dürig <mduerig@apache.org>
2014-12-10 17:32:44 +0000

    OAK-2343: Wrong handling of InterruptedException in BackgroundThread Set the current's thread interrupted status
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1644490 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c35cda9db3f87b7479a85f8b2dbd79cfd378126f

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/BackgroundThread.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/BackgroundThread.java
@@ -267,6 +267,14 @@ name|InterruptedException
 name|e
 parameter_list|)
 block|{
+name|Thread
+operator|.
+name|currentThread
+argument_list|()
+operator|.
+name|interrupt
+argument_list|()
+expr_stmt|;
 name|log
 operator|.
 name|error
