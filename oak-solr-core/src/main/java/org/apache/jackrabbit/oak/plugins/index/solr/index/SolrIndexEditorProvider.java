13221866d87a6ae8c989e9508ae3218c644e6d2a
Tommaso Teofili <tommaso@apache.org>
2014-12-18 14:40:53 +0000

    OAK-2168 - support qt aggregation (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1646469 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c5f88472ec619174054f9f1f75c240cdec684230

--- oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/index/SolrIndexEditorProvider.java
+++ oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/index/SolrIndexEditorProvider.java
@@ -25,6 +25,16 @@ end_package
 
 begin_import
 import|import
+name|javax
+operator|.
+name|annotation
+operator|.
+name|Nonnull
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -228,16 +238,6 @@ operator|.
 name|slf4j
 operator|.
 name|LoggerFactory
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|annotation
-operator|.
-name|Nonnull
 import|;
 end_import
 
