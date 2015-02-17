c7d7979fce46bed2e529bbe730560b2c447739fd
Angela Schreiber <angela@apache.org>
2015-02-17 14:01:34 +0000

    OAK-2225 : MultipleMoveTest doesn't restore ACEs properly causing later tests to fail NA : Additional user management related test
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1660382 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 24c5e6ba49e33cb9d0c52dc73a9ef2831decd159

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/MultipleMoveTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/MultipleMoveTest.java
@@ -89,6 +89,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -341,22 +351,75 @@ block|{
 comment|// success
 block|}
 block|}
-comment|// FIXME OAK-2225 AbstractEvaluationTest doesn't restore ACEs properly causing later tests to fail
-comment|//    @Test
-comment|//    public void testMoveSubTreeBack2() throws Exception {
-comment|//        allow(testRootNode.getPath(), privilegesFromNames(new String[]{
-comment|//                Privilege.JCR_REMOVE_NODE,
-comment|//                Privilege.JCR_REMOVE_CHILD_NODES,
-comment|//                Privilege.JCR_ADD_CHILD_NODES,
-comment|//                Privilege.JCR_NODE_TYPE_MANAGEMENT
-comment|//        }));
-comment|//
-comment|//        first move must succeed
-comment|//        move(childNPath, siblingDestPath);
-comment|//        moving child back must fail due to missing privileges
-comment|//        move(siblingDestPath + '/' + nodeName3, path + "/subtreeBack");
-comment|//        testSession.save();
-comment|//    }
+annotation|@
+name|Test
+specifier|public
+name|void
+name|testMoveSubTreeBack2
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|allow
+argument_list|(
+name|testRootNode
+operator|.
+name|getPath
+argument_list|()
+argument_list|,
+name|privilegesFromNames
+argument_list|(
+operator|new
+name|String
+index|[]
+block|{
+name|Privilege
+operator|.
+name|JCR_REMOVE_NODE
+block|,
+name|Privilege
+operator|.
+name|JCR_REMOVE_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_ADD_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_NODE_TYPE_MANAGEMENT
+block|}
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|//first move must succeed
+name|move
+argument_list|(
+name|childNPath
+argument_list|,
+name|siblingDestPath
+argument_list|)
+expr_stmt|;
+comment|//moving child back must fail due to missing privileges
+name|move
+argument_list|(
+name|siblingDestPath
+operator|+
+literal|'/'
+operator|+
+name|nodeName3
+argument_list|,
+name|path
+operator|+
+literal|"/subtreeBack"
+argument_list|)
+expr_stmt|;
+name|testSession
+operator|.
+name|save
+argument_list|()
+expr_stmt|;
+block|}
 annotation|@
 name|Test
 specifier|public
@@ -413,23 +476,78 @@ block|{
 comment|// success
 block|}
 block|}
-comment|// FIXME OAK-2225 AbstractEvaluationTest doesn't restore ACEs properly causing later tests to fail
-comment|//    @Ignore("Known Limitation of OAK-710")
-comment|//    @Test
-comment|//    public void testMoveSubTreeBack4() throws Exception {
-comment|//        allow(testRootNode.getPath(), privilegesFromNames(new String[]{
-comment|//                Privilege.JCR_REMOVE_NODE,
-comment|//                Privilege.JCR_REMOVE_CHILD_NODES,
-comment|//                Privilege.JCR_ADD_CHILD_NODES,
-comment|//                Privilege.JCR_NODE_TYPE_MANAGEMENT
-comment|//        }));
-comment|//
-comment|//        first move must succeed
-comment|//        move(childNPath, siblingDestPath);
-comment|//        moving child back must fail due to missing privileges
-comment|//        move(siblingDestPath + '/' + nodeName3, childNPath);
-comment|//        testSession.save();
-comment|//    }
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Known Limitation of OAK-710"
+argument_list|)
+annotation|@
+name|Test
+specifier|public
+name|void
+name|testMoveSubTreeBack4
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|allow
+argument_list|(
+name|testRootNode
+operator|.
+name|getPath
+argument_list|()
+argument_list|,
+name|privilegesFromNames
+argument_list|(
+operator|new
+name|String
+index|[]
+block|{
+name|Privilege
+operator|.
+name|JCR_REMOVE_NODE
+block|,
+name|Privilege
+operator|.
+name|JCR_REMOVE_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_ADD_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_NODE_TYPE_MANAGEMENT
+block|}
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|//first move must succeed
+name|move
+argument_list|(
+name|childNPath
+argument_list|,
+name|siblingDestPath
+argument_list|)
+expr_stmt|;
+comment|//moving child back must fail due to missing privileges
+name|move
+argument_list|(
+name|siblingDestPath
+operator|+
+literal|'/'
+operator|+
+name|nodeName3
+argument_list|,
+name|childNPath
+argument_list|)
+expr_stmt|;
+name|testSession
+operator|.
+name|save
+argument_list|()
+expr_stmt|;
+block|}
 annotation|@
 name|Test
 specifier|public
@@ -484,23 +602,76 @@ block|{
 comment|// success
 block|}
 block|}
-comment|// FIXME OAK-2225 AbstractEvaluationTest doesn't restore ACEs properly causing later tests to fail
-comment|//    @Ignore("Known Limitation of OAK-710")
-comment|//    @Test
-comment|//    public void testMoveDestParent2() throws Exception {
-comment|//        allow(testRootNode.getPath(), privilegesFromNames(new String[]{
-comment|//                Privilege.JCR_REMOVE_NODE,
-comment|//                Privilege.JCR_REMOVE_CHILD_NODES,
-comment|//                Privilege.JCR_ADD_CHILD_NODES,
-comment|//                Privilege.JCR_NODE_TYPE_MANAGEMENT
-comment|//        }));
-comment|//
-comment|//        first move must succeed
-comment|//        move(childNPath, siblingDestPath);
-comment|//        moving dest parent to original source location
-comment|//        move(siblingPath, path + "/parentMove");
-comment|//        testSession.save();
-comment|//    }
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Known Limitation of OAK-710"
+argument_list|)
+annotation|@
+name|Test
+specifier|public
+name|void
+name|testMoveDestParent2
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|allow
+argument_list|(
+name|testRootNode
+operator|.
+name|getPath
+argument_list|()
+argument_list|,
+name|privilegesFromNames
+argument_list|(
+operator|new
+name|String
+index|[]
+block|{
+name|Privilege
+operator|.
+name|JCR_REMOVE_NODE
+block|,
+name|Privilege
+operator|.
+name|JCR_REMOVE_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_ADD_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_NODE_TYPE_MANAGEMENT
+block|}
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|//first move must succeed
+name|move
+argument_list|(
+name|childNPath
+argument_list|,
+name|siblingDestPath
+argument_list|)
+expr_stmt|;
+comment|//moving dest parent to original source location
+name|move
+argument_list|(
+name|siblingPath
+argument_list|,
+name|path
+operator|+
+literal|"/parentMove"
+argument_list|)
+expr_stmt|;
+name|testSession
+operator|.
+name|save
+argument_list|()
+expr_stmt|;
+block|}
 annotation|@
 name|Test
 specifier|public
@@ -553,23 +724,74 @@ block|{
 comment|// success
 block|}
 block|}
-comment|// FIXME OAK-2225 AbstractEvaluationTest doesn't restore ACEs properly causing later tests to fail
-comment|//    @Ignore("Known Limitation of OAK-710")
-comment|//    @Test
-comment|//    public void testMoveDestParent4() throws Exception {
-comment|//        allow(testRootNode.getPath(), privilegesFromNames(new String[]{
-comment|//                Privilege.JCR_REMOVE_NODE,
-comment|//                Privilege.JCR_REMOVE_CHILD_NODES,
-comment|//                Privilege.JCR_ADD_CHILD_NODES,
-comment|//                Privilege.JCR_NODE_TYPE_MANAGEMENT
-comment|//        }));
-comment|//
-comment|//        first move must succeed
-comment|//        move(childNPath, siblingDestPath);
-comment|//        moving dest parent to original source location
-comment|//        move(siblingPath, childNPath);
-comment|//        testSession.save();
-comment|//    }
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Known Limitation of OAK-710"
+argument_list|)
+annotation|@
+name|Test
+specifier|public
+name|void
+name|testMoveDestParent4
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|allow
+argument_list|(
+name|testRootNode
+operator|.
+name|getPath
+argument_list|()
+argument_list|,
+name|privilegesFromNames
+argument_list|(
+operator|new
+name|String
+index|[]
+block|{
+name|Privilege
+operator|.
+name|JCR_REMOVE_NODE
+block|,
+name|Privilege
+operator|.
+name|JCR_REMOVE_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_ADD_CHILD_NODES
+block|,
+name|Privilege
+operator|.
+name|JCR_NODE_TYPE_MANAGEMENT
+block|}
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|//first move must succeed
+name|move
+argument_list|(
+name|childNPath
+argument_list|,
+name|siblingDestPath
+argument_list|)
+expr_stmt|;
+comment|//moving dest parent to original source location
+name|move
+argument_list|(
+name|siblingPath
+argument_list|,
+name|childNPath
+argument_list|)
+expr_stmt|;
+name|testSession
+operator|.
+name|save
+argument_list|()
+expr_stmt|;
+block|}
 block|}
 end_class
 
