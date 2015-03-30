f6f9aaa4dabfb3396b7cf0e49d3fa036a1980ad3
Angela Schreiber <angela@apache.org>
2015-03-30 14:05:46 +0000

    OAK-2674 : Fix FindBug Issues (Unused Imports, add @SuppressWarning annotation)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1670101 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 056733d999a9a38b474d7bce9922335f519c7b80

--- oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/query/FilterQueryParser.java
+++ oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/query/FilterQueryParser.java
@@ -45,16 +45,6 @@ end_import
 
 begin_import
 import|import
-name|javax
-operator|.
-name|jcr
-operator|.
-name|PropertyType
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
