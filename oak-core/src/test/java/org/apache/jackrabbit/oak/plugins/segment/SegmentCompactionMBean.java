4de9f4b9beb3ac40a1fd14565ee7e32a2c11648f
Michael Dürig <mduerig@apache.org>
2015-04-15 14:41:02 +0000

    OAK-2713: High memory usage of CompactionMap IT for monitoring GC and simulating various conditions through JMX
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1673787 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 822cfc39f7c8105228230570180c59bc55d1cc32

---
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/segment/SegmentCompactionMBean.java
@@ -0,0 +1,171 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *   http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
+name|plugins
+operator|.
+name|segment
+package|;
+end_package
+
+begin_comment
+comment|/**  * MBean for monitoring and interacting with the {@link SegmentCompactionIT}  * longevity test.  */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|SegmentCompactionMBean
+block|{
+comment|/**      * Stop the test.      */
+name|void
+name|stop
+parameter_list|()
+function_decl|;
+comment|/**      * Set the compaction interval      * @param minutes  number of minutes to wait between compaction cycles.      */
+name|void
+name|setCompactionInterval
+parameter_list|(
+name|int
+name|minutes
+parameter_list|)
+function_decl|;
+comment|/**      * @return  the compaction interval in minutes.      */
+name|int
+name|getCompactionInterval
+parameter_list|()
+function_decl|;
+comment|/**      * @return  Time stamp from when compaction last ran.      */
+name|String
+name|getLastCompaction
+parameter_list|()
+function_decl|;
+comment|/**      * Set the maximal number of concurrent readers      * @param count      */
+name|void
+name|setMaxReaders
+parameter_list|(
+name|int
+name|count
+parameter_list|)
+function_decl|;
+comment|/**      * @return  maximal number of concurrent readers      */
+name|int
+name|getMaxReaders
+parameter_list|()
+function_decl|;
+comment|/**      * Set the maximal number of concurrent writers      * @param count      */
+name|void
+name|setMaxWriters
+parameter_list|(
+name|int
+name|count
+parameter_list|)
+function_decl|;
+comment|/**      * @return  maximal number of concurrent writers      */
+name|int
+name|getMaxWriters
+parameter_list|()
+function_decl|;
+comment|/**      * Set the maximal size of the store      * @param size  size in bytes      */
+name|void
+name|setMaxStoreSize
+parameter_list|(
+name|long
+name|size
+parameter_list|)
+function_decl|;
+comment|/**      * @return  maximal size of the store in bytes      */
+name|long
+name|getMaxStoreSize
+parameter_list|()
+function_decl|;
+comment|/**      * Set the maximal size of binary properties      * @param size  size in bytes      */
+name|void
+name|setMaxBlobSize
+parameter_list|(
+name|int
+name|size
+parameter_list|)
+function_decl|;
+comment|/**      * @return  maximal size of binary properties in bytes      */
+name|int
+name|getMaxBlobSize
+parameter_list|()
+function_decl|;
+comment|/**      * Set the maximal number of held references      * @param count  maximal number of references      */
+name|void
+name|setMaxReferences
+parameter_list|(
+name|int
+name|count
+parameter_list|)
+function_decl|;
+comment|/**      * @return  maximal number of held references      */
+name|int
+name|getMaxReferences
+parameter_list|()
+function_decl|;
+comment|/**      * Add a reference to the current root or release a held reference.      * @param set  add a reference if {@code true}, otherwise release any held reference      */
+name|void
+name|setRootReference
+parameter_list|(
+name|boolean
+name|set
+parameter_list|)
+function_decl|;
+comment|/**      * @return  {@code true} if currently a root reference is being held. {@code false} otherwise.      */
+name|boolean
+name|getRootReference
+parameter_list|()
+function_decl|;
+comment|/**      * @return  actual number of concurrent readers      */
+name|int
+name|getReaderCount
+parameter_list|()
+function_decl|;
+comment|/**      * @return  actual number of concurrent writers      */
+name|int
+name|getWriterCount
+parameter_list|()
+function_decl|;
+comment|/**      * @return actual number of held references (not including any root reference)      */
+name|int
+name|getReferenceCount
+parameter_list|()
+function_decl|;
+comment|/**      * @return  current size of the {@link org.apache.jackrabbit.oak.plugins.segment.file.FileStore}      */
+name|long
+name|getFileStoreSize
+parameter_list|()
+function_decl|;
+comment|/**      * @return  current weight of the compaction map      */
+name|long
+name|getCompactionMapWeight
+parameter_list|()
+function_decl|;
+comment|/**      * @return  current depth of the compaction map      */
+name|int
+name|getCompactionMapDepth
+parameter_list|()
+function_decl|;
+comment|/**      * @return  last error      */
+name|String
+name|getLastError
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
