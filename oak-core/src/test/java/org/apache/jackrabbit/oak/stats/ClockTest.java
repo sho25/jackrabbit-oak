1a40dae1b59b1d3ca1a47440c41d2919af367e5f
Julian Reschke <reschke@apache.org>
2015-06-08 13:01:26 +0000

    undo @ignore on ClockTest introduced in r1684174
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1684175 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c1f94b01bc9b350c5dd52581bd1c3c9f5f33aba2

--- oak-core/src/test/java/org/apache/jackrabbit/oak/stats/ClockTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/stats/ClockTest.java
@@ -87,16 +87,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -198,8 +188,6 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
-annotation|@
-name|Ignore
 specifier|public
 name|void
 name|testClockDriftFast
