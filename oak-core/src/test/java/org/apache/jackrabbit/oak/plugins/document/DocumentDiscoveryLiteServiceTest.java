52ab66a742f6247e0e9043e652bb8184645be3ec
Stefan Egli <stefanegli@apache.org>
2015-08-24 14:24:50 +0000

    OAK-2844 - disabling DocumentDiscoveryLiteServiceTest until user.dir issue fixed
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1697407 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0c0d267bd97b42b298c612ff11cfff8bd5da27e4

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentDiscoveryLiteServiceTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentDiscoveryLiteServiceTest.java
@@ -577,6 +577,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -702,6 +712,11 @@ comment|/**  * Tests for the DocumentDiscoveryLiteService  */
 end_comment
 
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"OAK-2844 : user.dir trick messes up with the other tests"
+argument_list|)
 specifier|public
 class|class
 name|DocumentDiscoveryLiteServiceTest
