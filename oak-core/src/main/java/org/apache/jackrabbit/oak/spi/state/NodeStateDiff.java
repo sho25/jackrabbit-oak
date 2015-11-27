170a387ad53e1377e29503273ea21615595814c5
Thomas Mueller <thomasm@apache.org>
2015-11-27 09:30:27 +0000

    OAK-301 Document Oak
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1716810 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: da10762cc02c92ecc654ca3a9699597fe4cdc5f2

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/state/NodeStateDiff.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/state/NodeStateDiff.java
@@ -36,7 +36,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Handler of node state differences. The  * {@link NodeState#compareAgainstBaseState(NodeState, NodeStateDiff)}  * method reports detected node state differences by calling methods of  * a handler instance that implements this interface.  *<p>  * The compare method goes through all properties and child nodes of the  * two states, calling the relevant added, changed or deleted methods  * where appropriate. Differences in the ordering of properties or child  * nodes do not affect the comparison, and the order in which such  * differences are reported is unspecified.  *<p>  * The methods in this interface all return a boolean value to signify  * whether the comparison should continue. If a method returns {@code false},  * then the comparison is immediately stopped. Otherwise it continues until  * all changes have been reported.  *<p>  * Note that the  * {@link NodeState#compareAgainstBaseState(NodeState, NodeStateDiff)}  * method only compares the given states without recursing to the subtrees  * below. An implementation of this interface should recursively call that  * method for the relevant child node entries to find out all the changes  * across the entire subtree below the given node.  */
+comment|/**  * Handler of node state differences. The  * {@link NodeState#compareAgainstBaseState(NodeState, NodeStateDiff)}  * method reports detected node state differences by calling methods of  * a handler instance that implements this interface.  *<p>  * The compare method goes through all properties and child nodes of the  * two states, calling the relevant added, changed or deleted methods  * where appropriate. Differences in the ordering of properties or child  * nodes do not affect the comparison, and the order in which such  * differences are reported is unspecified.  *<p>  * The methods in this interface all return a boolean value to signify whether  * the comparison should continue or abort. If a method returns {@code false},  * then the comparison is immediately stopped, that means sibling nodes and  * sibling nodes of all parents are not further processed. Otherwise it  * continues until all changes have been reported.  *<p>  * Note that the  * {@link NodeState#compareAgainstBaseState(NodeState, NodeStateDiff)}  * method only compares the given states without recursing to the subtrees  * below. An implementation of this interface should recursively call that  * method for the relevant child node entries to find out all the changes  * across the entire subtree below the given node.  */
 end_comment
 
 begin_interface
@@ -44,7 +44,7 @@ specifier|public
 interface|interface
 name|NodeStateDiff
 block|{
-comment|/**      * Called for all added properties.      *      * @param after property state after the change      * @return {@code true} to continue the comparison, {@code false} to stop      */
+comment|/**      * Called for all added properties.      *      * @param after property state after the change      * @return {@code true} to continue the comparison, {@code false} to abort.      *         Abort will stop comparing completely, that means sibling nodes      *         and sibling nodes of all parents are not further compared.      */
 name|boolean
 name|propertyAdded
 parameter_list|(
@@ -52,7 +52,7 @@ name|PropertyState
 name|after
 parameter_list|)
 function_decl|;
-comment|/**      * Called for all changed properties. The names of the given two      * property states are guaranteed to be the same.      *      * @param before property state before the change      * @param after property state after the change      * @return {@code true} to continue the comparison, {@code false} to stop      */
+comment|/**      * Called for all changed properties. The names of the given two      * property states are guaranteed to be the same.      *      * @param before property state before the change      * @param after property state after the change      * @return {@code true} to continue the comparison, {@code false} to abort.      *         Abort will stop comparing completely, that means sibling nodes      *         and sibling nodes of all parents are not further compared.      */
 name|boolean
 name|propertyChanged
 parameter_list|(
@@ -63,7 +63,7 @@ name|PropertyState
 name|after
 parameter_list|)
 function_decl|;
-comment|/**      * Called for all deleted properties.      *      * @param before property state before the change      * @return {@code true} to continue the comparison, {@code false} to stop      */
+comment|/**      * Called for all deleted properties.      *      * @param before property state before the change      * @return {@code true} to continue the comparison, {@code false} to abort.      *         Abort will stop comparing completely, that means sibling nodes      *         and sibling nodes of all parents are not further compared.      */
 name|boolean
 name|propertyDeleted
 parameter_list|(
@@ -71,7 +71,7 @@ name|PropertyState
 name|before
 parameter_list|)
 function_decl|;
-comment|/**      * Called for all added child nodes.      *      * @param name name of the added child node      * @param after child node state after the change      * @return {@code true} to continue the comparison, {@code false} to stop      */
+comment|/**      * Called for all added child nodes.      *      * @param name name of the added child node      * @param after child node state after the change      * @return {@code true} to continue the comparison, {@code false} to abort.      *         Abort will stop comparing completely, that means sibling nodes      *         and sibling nodes of all parents are not further compared.      */
 name|boolean
 name|childNodeAdded
 parameter_list|(
@@ -82,7 +82,7 @@ name|NodeState
 name|after
 parameter_list|)
 function_decl|;
-comment|/**      * Called for all child nodes that may contain changes between the before      * and after states. The comparison implementation is expected to make an      * effort to avoid calling this method on child nodes under which nothing      * has changed.      *      * @param name name of the changed child node      * @param before child node state before the change      * @param after child node state after the change      * @return {@code true} to continue the comparison, {@code false} to stop      */
+comment|/**      * Called for all child nodes that may contain changes between the before      * and after states. The comparison implementation is expected to make an      * effort to avoid calling this method on child nodes under which nothing      * has changed.      *      * @param name name of the changed child node      * @param before child node state before the change      * @param after child node state after the change      * @return {@code true} to continue the comparison, {@code false} to abort.      *         Abort will stop comparing completely, that means sibling nodes      *         and sibling nodes of all parents are not further compared.      */
 name|boolean
 name|childNodeChanged
 parameter_list|(
@@ -96,7 +96,7 @@ name|NodeState
 name|after
 parameter_list|)
 function_decl|;
-comment|/**      * Called for all deleted child nodes.      *      * @param name name of the deleted child node      * @param before child node state before the change      * @return {@code true} to continue the comparison, {@code false} to stop      */
+comment|/**      * Called for all deleted child nodes.      *      * @param name name of the deleted child node      * @param before child node state before the change      * @return {@code true} to continue the comparison, {@code false} to abort.      *         Abort will stop comparing completely, that means sibling nodes      *         and sibling nodes of all parents are not further compared.      */
 name|boolean
 name|childNodeDeleted
 parameter_list|(
