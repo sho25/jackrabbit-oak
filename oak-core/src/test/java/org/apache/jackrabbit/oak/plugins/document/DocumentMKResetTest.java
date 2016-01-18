dd6b3d10280940a7f4051378f77c86dea42815d6
Marcel Reutegger <mreutegg@apache.org>
2016-01-18 09:01:17 +0000

    OAK-98 Source code formatting, code conventions, Javadocs
    Enable test
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1725193 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7b635d4ccc6d896f1364a3d9aea3b87bb47ee858

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentMKResetTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentMKResetTest.java
@@ -25,16 +25,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -519,8 +509,6 @@ literal|"/bar"
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Ignore
 annotation|@
 name|Test
 specifier|public
