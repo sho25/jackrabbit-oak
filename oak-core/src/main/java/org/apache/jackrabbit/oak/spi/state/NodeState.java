1d4e1f83e01852504568fe67fee889dfd247cb04
Michael Dürig <mduerig@apache.org>
2016-03-08 20:11:00 +0000

    @trivial: convert tab to spaces
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1734139 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: edf0adcf37bb55bd6fb438a391ada4b67033654f

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/state/NodeState.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/state/NodeState.java
@@ -115,7 +115,7 @@ name|String
 name|name
 parameter_list|)
 function_decl|;
-comment|/**      * Returns the boolean value of the named property. The implementation      * is equivalent to the following code, but may be optimized.      *<pre> 	 * {@code      * PropertyState property = state.getProperty(name);      * return property != null      *&& property.getType() == Type.BOOLEAN      *&& property.getValue(Type.BOOLEAN);      * }      *</pre>      *      * @param name property name      * @return boolean value of the named property, or {@code false}      */
+comment|/**      * Returns the boolean value of the named property. The implementation      * is equivalent to the following code, but may be optimized.      *<pre>      * {@code      * PropertyState property = state.getProperty(name);      * return property != null      *&& property.getType() == Type.BOOLEAN      *&& property.getValue(Type.BOOLEAN);      * }      *</pre>      *      * @param name property name      * @return boolean value of the named property, or {@code false}      */
 name|boolean
 name|getBoolean
 parameter_list|(
