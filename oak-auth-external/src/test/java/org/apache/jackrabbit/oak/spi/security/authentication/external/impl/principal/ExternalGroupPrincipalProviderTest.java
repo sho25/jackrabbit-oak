1097ec7d064fa5c656427cef5c4eae972f0500a2
Angela Schreiber <angela@apache.org>
2016-06-06 09:12:47 +0000

    minor improvement: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1746978 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6aaf660cb9aa59c9ca60c7fdf10620a7a3d11120

--- oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/impl/principal/ExternalGroupPrincipalProviderTest.java
+++ oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/impl/principal/ExternalGroupPrincipalProviderTest.java
@@ -519,18 +519,6 @@ name|assertTrue
 import|;
 end_import
 
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|fail
-import|;
-end_import
-
 begin_class
 specifier|public
 class|class
