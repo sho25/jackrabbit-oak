5389a3ba8b86555cae84a78ec8c1cd629df5332f
Michael Dürig <mduerig@apache.org>
2017-01-26 17:04:19 +0000

    OAK-5517: SNFE when running compaction after a cancelled gc Correctly evict the deduplication cache for nodes whenever a compaction run is aborted
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1780429 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c0ea1ed1c7769752d72762704c02bda7c2c5d735

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/CompactionAndCleanupIT.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/CompactionAndCleanupIT.java
@@ -831,16 +831,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Rule
 import|;
 end_import
@@ -3737,12 +3727,6 @@ block|}
 block|}
 block|}
 comment|/**      * See OAK-5517: SNFE when running compaction after a cancelled gc      */
-annotation|@
-name|Ignore
-argument_list|(
-literal|"OAK-5517"
-argument_list|)
-comment|// FIXME OAK-5517: SNFE when running compaction after a cancelled gc
 annotation|@
 name|Test
 specifier|public
