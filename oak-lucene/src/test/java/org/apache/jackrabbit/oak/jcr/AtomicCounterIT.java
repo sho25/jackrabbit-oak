8f7258b740d191e8391eadb7b398909e29847c1c
Marcel Reutegger <mreutegg@apache.org>
2017-05-03 08:44:45 +0000

    OAK-6149: AtomicCounter fails with LuceneIndexEditorProvider
    Create CommitInfo for merge in consolidation task and enable test
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1793627 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0345c531f2418d176c682c1b4b7f3eda41f6ca54

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/AtomicCounterIT.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/AtomicCounterIT.java
@@ -407,16 +407,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Rule
 import|;
 end_import
@@ -622,11 +612,6 @@ import|;
 end_import
 
 begin_class
-annotation|@
-name|Ignore
-argument_list|(
-literal|"OAK-6149"
-argument_list|)
 specifier|public
 class|class
 name|AtomicCounterIT
