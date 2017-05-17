536211753bcb47a95b62172aa7b07edbfe87f1ea
Marcel Reutegger <mreutegg@apache.org>
2017-05-17 06:35:12 +0000

    OAK-3712: Clean up uncommitted changes
    Sweeper uses incorrect lower bound for _modified
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1795400 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8f2b41503b3c8baada003952eab28fca1a516967

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/MissingLastRevSeeker.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/MissingLastRevSeeker.java
@@ -496,7 +496,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**      * Get the candidates with modified time greater than or equal the specified      * {@code startTime}.      *      * @param startTime the start time.      * @return the candidates      */
+comment|/**      * Get the candidates with modified time greater than or equal the specified      * {@code startTime} in milliseconds since the start of the epoch.      *      * @param startTime the start time in milliseconds.      * @return the candidates      */
 annotation|@
 name|Nonnull
 specifier|public
