118b5ac18a9ecda57972db206c92dd2c4d45e02a
Tomasz Rekawek <tomekr@apache.org>
2017-08-27 13:54:08 +0000

    OAK-6592: Remove path and rootBuilder from the CompositeNodeBuilder
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1806367 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3d265f7583d4caf8c496fb3c7401118a1cca0edb

--- oak-it/src/test/java/org/apache/jackrabbit/oak/composite/CompositeNodeStoreTest.java
+++ oak-it/src/test/java/org/apache/jackrabbit/oak/composite/CompositeNodeStoreTest.java
@@ -935,6 +935,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -4494,6 +4504,11 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Test ignored, since only the default store is writeable"
+argument_list|)
 specifier|public
 name|void
 name|moveNodeBetweenStores
