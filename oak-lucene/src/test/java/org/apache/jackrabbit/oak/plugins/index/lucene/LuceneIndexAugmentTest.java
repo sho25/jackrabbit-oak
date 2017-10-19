9fb6788caeb0f198b581e3f0aa201b2a1c693650
Julian Reschke <reschke@apache.org>
2017-10-19 17:30:34 +0000

    OAK-6833: LuceneIndex*Test failures
    Disable LuceneIndexAugmentTest.nullBehavior on Windows
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1812662 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a30c49a686cb7302c79e96cf8e1021388c9709bd

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAugmentTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAugmentTest.java
@@ -133,6 +133,22 @@ name|jackrabbit
 operator|.
 name|oak
 operator|.
+name|commons
+operator|.
+name|CIHelper
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|jackrabbit
+operator|.
+name|oak
+operator|.
 name|plugins
 operator|.
 name|index
@@ -558,6 +574,18 @@ operator|.
 name|Assert
 operator|.
 name|assertTrue
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assume
+operator|.
+name|assumeFalse
 import|;
 end_import
 
@@ -1531,6 +1559,15 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+comment|// OAK-6833
+name|assumeFalse
+argument_list|(
+name|CIHelper
+operator|.
+name|windows
+argument_list|()
+argument_list|)
+expr_stmt|;
 comment|//setup repo and index
 name|NodeTypeRegistry
 operator|.
