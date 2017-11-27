70415b541261d74093b43409cdbb127e9d512acc
Francesco Mari <frm@apache.org>
2017-11-27 08:47:16 +0000

    OAK-6969 - Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1816416 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: dad0f939c8ad42ef91f6ec2eef443e92c74b59b3

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/tool/Compact.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/tool/Compact.java
@@ -285,20 +285,6 @@ end_import
 
 begin_import
 import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|base
-operator|.
-name|Supplier
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
