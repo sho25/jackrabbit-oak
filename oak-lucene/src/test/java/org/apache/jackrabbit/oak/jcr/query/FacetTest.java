ff026725f23d8101131147ef72e013befc86267c
Vikas Saurabh <catholicon@apache.org>
2017-12-22 15:41:28 +0000

    OAK-7078: NullPointerException in FilteredSortedSetDocValuesFacetCounts during query evaluation
    Applied fix from Dirk Rudolph ([~diru]) and a bit of modified test from
    his patch in r1819048. Thanks Dirk for the contribution.
    Removing an unused import here.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1819050 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b7f274a595df895ad3330bc9c611cc705249eb3f

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/query/FacetTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/query/FacetTest.java
@@ -113,16 +113,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|ArrayList
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|List
 import|;
 end_import
