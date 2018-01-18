1a4e85720fa4101ea969e393dd2a3972cac3195b
Marcel Reutegger <mreutegg@apache.org>
2018-01-18 11:36:29 +0000

    OAK-7176: RevisionVector from empty string throws StringIndexOutOfBoundsException
    Fix and enable test
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1821487 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aca00433f5f1f6039b27d5c5c7748450eb1f6ca6

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/RevisionVectorTest.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/RevisionVectorTest.java
@@ -53,16 +53,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -3347,8 +3337,6 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Ignore
 annotation|@
 name|Test
 specifier|public
