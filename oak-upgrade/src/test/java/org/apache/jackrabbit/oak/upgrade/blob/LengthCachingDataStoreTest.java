c766933c0dd57fa52fa1306e1aa3f96ccf878d01
Julian Reschke <reschke@apache.org>
2018-01-22 12:00:55 +0000

    OAK-7184: LengthCachingDataStoreTest: Remove unused import of Guava InputSupplier
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1821847 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3eab6401c943ee8c5948e375eca1091e2fdda2e6

--- oak-upgrade/src/test/java/org/apache/jackrabbit/oak/upgrade/blob/LengthCachingDataStoreTest.java
+++ oak-upgrade/src/test/java/org/apache/jackrabbit/oak/upgrade/blob/LengthCachingDataStoreTest.java
@@ -167,20 +167,6 @@ end_import
 
 begin_import
 import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|io
-operator|.
-name|InputSupplier
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
