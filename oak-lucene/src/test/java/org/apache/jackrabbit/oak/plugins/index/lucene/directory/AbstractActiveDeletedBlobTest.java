48ff85b532192894850f69612c4120073fd7f91c
Julian Reschke <reschke@apache.org>
2018-01-23 09:49:39 +0000

    OAK-7186: avoid use of guava Iterators.emptyIterator()
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1821980 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 593d777eed6a8261b611939b719f75580caf867c

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/directory/AbstractActiveDeletedBlobTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/directory/AbstractActiveDeletedBlobTest.java
@@ -59,6 +59,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|Collections
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|Iterator
 import|;
 end_import
@@ -1823,7 +1833,7 @@ argument_list|()
 return|;
 block|}
 return|return
-name|Iterators
+name|Collections
 operator|.
 name|emptyIterator
 argument_list|()
