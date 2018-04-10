84e1277348880fdb4b4c97f983bc208967280cad
Julian Reschke <reschke@apache.org>
2018-04-10 14:46:02 +0000

    OAK-7024: java.security.acl deprecated in Java 10, marked for removal in Java 12
    avoid use of Guava emptyIterator()
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1828822 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ac7f897d08ba9eec5458b352e13b4969b72d3a22

--- oak-security-spi/src/test/java/org/apache/jackrabbit/oak/spi/security/principal/CompositePrincipalConfigurationTest.java
+++ oak-security-spi/src/test/java/org/apache/jackrabbit/oak/spi/security/principal/CompositePrincipalConfigurationTest.java
@@ -49,6 +49,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|Collections
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|Iterator
 import|;
 end_import
@@ -114,20 +124,6 @@ operator|.
 name|collect
 operator|.
 name|ImmutableSet
-import|;
-end_import
-
-begin_import
-import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|collect
-operator|.
-name|Iterators
 import|;
 end_import
 
@@ -1036,7 +1032,7 @@ name|searchType
 parameter_list|)
 block|{
 return|return
-name|Iterators
+name|Collections
 operator|.
 name|emptyIterator
 argument_list|()
@@ -1060,7 +1056,7 @@ name|searchType
 parameter_list|)
 block|{
 return|return
-name|Iterators
+name|Collections
 operator|.
 name|emptyIterator
 argument_list|()
