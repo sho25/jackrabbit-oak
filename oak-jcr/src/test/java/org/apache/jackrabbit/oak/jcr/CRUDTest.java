761bba0ce146b4bb5c2a1a87a20fadae24961586
Marcel Reutegger <mreutegg@apache.org>
2018-07-23 13:42:07 +0000

    OAK-7652: Node.getMixinNodeTypes can revive deleted node mixins (again)
    Apply fix and enable test
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1836495 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c5c5df10f24cfe859482c863811363615af219e4

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/CRUDTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/CRUDTest.java
@@ -227,16 +227,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -415,11 +405,6 @@ argument_list|)
 expr_stmt|;
 block|}
 comment|// OAK-7652
-annotation|@
-name|Ignore
-argument_list|(
-literal|"OAK-7652"
-argument_list|)
 annotation|@
 name|Test
 specifier|public
