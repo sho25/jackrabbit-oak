9a85f87b363422d763a4753c5acfda164f336d1b
Julian Reschke <reschke@apache.org>
2019-04-05 14:46:34 +0000

    OAK-8111: Create read-only DocumentNodeStore for oak-run recovery dry run - remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1857010 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c9202a5168e42c800bee0a6964ad9312aedad319

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/RecoveryCommand.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/RecoveryCommand.java
@@ -313,24 +313,6 @@ end_import
 
 begin_import
 import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|jackrabbit
-operator|.
-name|oak
-operator|.
-name|spi
-operator|.
-name|state
-operator|.
-name|NodeStore
-import|;
-end_import
-
-begin_import
-import|import
 name|com
 operator|.
 name|google
