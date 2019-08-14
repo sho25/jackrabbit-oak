b7a79c568238ed2bfbe37b5d74dbd1a17b24f7e5
Marcel Reutegger <mreutegg@apache.org>
2019-08-14 12:03:56 +0000

    OAK-8538: Incomplete recovery on long running merge with branch commits
    Detect branch commits on recovery and enable tests
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1865110 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c221c0829cf7e3580db4ee381cc4ca0b9fde59d0

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/AddNodesInBranchCommitWithRecoveryTest.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/AddNodesInBranchCommitWithRecoveryTest.java
@@ -149,16 +149,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Rule
 import|;
 end_import
@@ -238,8 +228,6 @@ import|;
 end_import
 
 begin_class
-annotation|@
-name|Ignore
 specifier|public
 class|class
 name|AddNodesInBranchCommitWithRecoveryTest
