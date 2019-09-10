ec3a61b871278be86a6e059282ead32a6643685a
Marcel Reutegger <mreutegg@apache.org>
2019-09-10 08:00:30 +0000

    OAK-8591: Conflict exception on commit
    Apply proposed changes and enable tests
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1866730 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2d8c4ee7cc132f101eee024af80febb0c944ec02

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/NodeDocumentTest.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/NodeDocumentTest.java
@@ -377,16 +377,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -3625,8 +3615,6 @@ argument_list|()
 expr_stmt|;
 block|}
 annotation|@
-name|Ignore
-annotation|@
 name|Test
 specifier|public
 name|void
@@ -3641,8 +3629,6 @@ literal|false
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Ignore
 annotation|@
 name|Test
 specifier|public
