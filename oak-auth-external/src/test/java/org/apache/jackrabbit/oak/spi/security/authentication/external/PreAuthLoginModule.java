e41f0b2cae3696e9118fe8affd55fd103e6f3d54
Angela Schreiber <angela@apache.org>
2019-11-28 15:37:42 +0000

    OAK-8710 : AbstractLoginModule#logout() must not remove 'foreign' principals/credentials
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1870559 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 77b45e61afcb9adf9006b88e9f5fc5d22508ff36

--- oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/PreAuthLoginModule.java
+++ oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/PreAuthLoginModule.java
@@ -279,6 +279,17 @@ return|return
 literal|false
 return|;
 block|}
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|logout
+parameter_list|()
+block|{
+return|return
+literal|false
+return|;
+block|}
 block|}
 end_class
 
